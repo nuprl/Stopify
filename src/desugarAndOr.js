d452439f45ca4c82feb6c288bdeef72571ce1842
Sam Baxter <sbaxter@cs.umass.edu>
2017-03-24 11:27:18 -0400

    Formatting and comments for desugarAndOr

--- src/desugarAndOr.js
+++ src/desugarAndOr.js
@@ -1,12 +1,15 @@
 /**
  * Module to desugar early-escaping && and || operators.
+ *
+ * Early-escaping logical expressions are transformed into ternary expressions.
  */
 
 const t = require('babel-types');
-//
+
 // Object containing the visitor functions
 const visitor = {};
 
+// No Logical Binary Expressions
 visitor.LogicalExpression = function (path) {
   const node = path.node;
   const { left, operator, right } = node;
@@ -15,10 +18,13 @@ visitor.LogicalExpression = function (path) {
   const test = t.sequenceExpression([
     t.assignmentExpression('=', tmp, left),
     tmp]);
-  const trueBranch = t.assignmentExpression('=', tmp, operator === '&&' ? right : left);
-  const falseBranch = t.assignmentExpression('=', tmp, operator === '&&' ? left : right);
+  const trueBranch = t.assignmentExpression('=', tmp,
+      operator === '&&' ? right : left);
+  const falseBranch = t.assignmentExpression('=', tmp,
+      operator === '&&' ? left : right);
 
-  path.getStatementParent().insertBefore(t.variableDeclaration('let', [t.variableDeclarator(tmp)]));
+  path.getStatementParent().insertBefore(t.variableDeclaration('let',
+      [t.variableDeclarator(tmp)]));
   path.replaceWith(t.ifStatement(test,
         t.expressionStatement(trueBranch),
         t.expressionStatement(falseBranch)));

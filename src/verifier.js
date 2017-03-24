622406205aaf71f0834016281c448f37e9b257cd
thEnigma <rachit.nigam12@gmail.com>
2017-03-23 21:22:50 -0400

    Add a verifier "transform" for tests.

---
+++ src/verifier.js
@@ -0,0 +1,20 @@
+// Use this file to document all the constraints on JS--.
+const t = require('babel-types');
+
+const visitor = {}
+
+// Only while loops.
+visitor.Loop = function Loop(path) {
+  if (t.isWhileStatement(path.node) === false) {
+    throw new Error(`Resulting code has ${path.node.type}`);
+  }
+}
+
+// No switch-case constructs.
+visitor['SwitchStatement|SwitchCase'] = function (path) {
+  throw new Error(`Resulting code has ${path.node.type}`);
+}
+
+module.exports = function transform(babel) {
+  return { visitor };
+};

df77376b9d7ae14c480771ef408c6e3224487ba5
thEnigma <rachit.nigam12@gmail.com>
2017-03-30 09:41:54 -0400

    80 chars for comments

--- src/stratify.js
+++ src/stratify.js
@@ -1,19 +1,20 @@
-/**
- * Module to stratify a simplified javascript AST. Under our assumptions, programs can only diverge
- * at function call-sites. We name every function application to ease the implementation of further
- * transforming ASTs to insert checkpoints at each application.
+/** Module to stratify a simplified javascript AST. Under our assumptions,
+ * programs can only diverge at function call-sites. We name every function
+ * application to ease the implementation of further transforming ASTs to
+ * insert checkpoints at each application.
  *
- * We stratify an AST `e` into terms `m` that _must_ terminate, and terms `p` that are _possibly
- * non-termination_.
+ * We stratify an AST `e` into terms `m` that _must_ terminate, and terms `p`
+ * that are _possibly non-termination_.
  *
- * Must Terminate m ::=
- *   c | x | m_1 op m_2 | if (m_1) { m_2 } else { m_3 } | function(x,...) { p } | let x = m_1; m_2
+ * Must Terminate m ::= c | x | m_1 op m_2 | if (m_1) { m_2 } else { m_3 } |
+ * function(x,...) { p } | let x = m_1; m_2
  *
- * Possibly Nonterminating p ::=
- *   m | let x = m; p | let x = m_1(m_2,...); p | if (m_1) { p_1 } else { p_2 }
+ * Possibly Nonterminating p ::= m | let x = m; p | let x = m_1(m_2,...); p |
+ * if (m_1) { p_1 } else { p_2 }
  *
- * TODO(rachit & sam): This doesn't need to be recursive, we can just modify anf.js to do a minimal
- * anf transformation taking termination into consideration.
+ * TODO(rachit & sam): This doesn't need to be recursive, we can just modify
+ * anf.js to do a minimal anf transformation taking termination into
+ * consideration.
  */
 
 const t = require('babel-types');
@@ -21,11 +22,13 @@ const h = require('./helpers.js');
 
 const visitor = {};
 
-// stratifyExpression takes an Expression node, a continuation, and a path argument.
+// stratifyExpression takes an Expression node, a continuation, and a path
+// argument.
 //
-// It returns a pair of a _must_ terminate expression and a list of statements. These statements
-// are potential variable declarations that must be prepended to the expression's parent statement,
-// which is inaccessible via `path` in this recursive implementation.
+// It returns a pair of a _must_ terminate expression and a list of statements.
+// These statements are potential variable declarations that must be prepended
+// to the expression's parent statement, which is inaccessible via `path` in
+// this recursive implementation.
 function stratifyExpression(e, k, path) {
   if (t.isIdentifier(e)) {
     return [k(e), []];
@@ -64,7 +67,8 @@ function stratifyExpression(e, k, path) {
     let pres = [];
     const [exp, presCallee] = stratifyExpression(e.callee,
         (func) => {
-          // generate an identifier to which to bind the result of the application
+          // generate an identifier to which to bind the result of the
+          // application
           const tmp = path.scope.generateUidIdentifier('r');
           const presArgs = [];
           const args = e.arguments.map((arg) => {
@@ -74,8 +78,8 @@ function stratifyExpression(e, k, path) {
           });
           const c = t.callExpression(func, args);
           const v = h.letExpression(tmp, c);
-          // concat the new variableDeclaration to our accumulating list of decls to return to the
-          // top-level.
+          // concat the new variableDeclaration to our accumulating list of
+          // decls to return to the top-level.
           pres = pres.concat(presArgs);
           pres = pres.concat(v);
           return k(tmp);
@@ -88,8 +92,8 @@ function stratifyExpression(e, k, path) {
 
 // stratifyStatement takes a statement, continuation, and path as arguments.
 //
-// It returns an array of stratified statements, including any new variable declarations required
-// to stratify the statement argument.
+// It returns an array of stratified statements, including any new variable
+// declarations required to stratify the statement argument.
 function stratifyStatement(statement, k, path) {
   if (t.isExpressionStatement(statement)) {
     const [exp, pres] = stratifyExpression(statement.expression, k, path);
@@ -121,8 +125,9 @@ visitor.Program = function (path) {
     return stratifyStatement(s, x => x, path);
   }
 
-  // map over each statement in our program. stratifying each statement produces an array of
-  // statements, so we need to flatten this array of arrays of statements.
+  // map over each statement in our program. stratifying each statement
+  // produces an array of statements, so we need to flatten this array of
+  // arrays of statements.
   const stratifyBody = h.flatten(body.map(mapStratify));
   // replace the program body with the stratified body.
   node.body = stratifyBody;

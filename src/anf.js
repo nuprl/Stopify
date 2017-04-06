3eada854691228938b01008664ca5e7d961a2e09
thEnigma <rachit.nigam12@gmail.com>
2017-04-06 19:04:13 -0400

    Branches of if statements should be block statements.

--- src/anf.js
+++ src/anf.js
@@ -15,7 +15,7 @@ const visitor = {};
 
 visitor.CallExpression = function (path) {
   if (h.isConsoleLog(path.node.callee)) return;
-  
+
   const p = path.parent;
   // Name the function application if it is not already named.
   if (t.isVariableDeclarator(p) === false) {
@@ -26,6 +26,20 @@ visitor.CallExpression = function (path) {
     path.replaceWith(name);
   }
 };
+
+// Consequents and alternatives in if statements must always be blocked,
+// otherwise variable declaration get pulled outside the branch.
+visitor.IfStatement = function (path) {
+  const { consequent, alternate } = path.node;
+
+  if (t.isBlockStatement(consequent) === false) {
+    path.node.consequent = t.blockStatement([consequent]);
+  }
+
+  if (alternate !== null && t.isBlockStatement(alternate) === false) {
+    path.node.alternate = t.blockStatement([alternate]);
+  }
+}
 
 module.exports = function transform(babel) {
   return { visitor };

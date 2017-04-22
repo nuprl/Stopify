d47c58200b1b3ccda307b207bd66821b1a5e713a
Rachit Nigam <rachit.nigam12@gmail.com>
2017-04-22 12:37:11 -0400

    Generate setTimeout in run function. - Can't return values from setTimeout, so new transform. - Wrap entire program into function and run that instead. - Fixes #4

--- src/yield.ts
+++ src/yield.ts
@@ -3,16 +3,12 @@ import * as t from 'babel-types';
 import * as b from 'babylon';
 import * as h from './helpers';
 
-interface CallExpression extends t.CallExpression {
-  dontTransform: boolean;
-};
 type Function = t.FunctionDeclaration|t.FunctionExpression;
 
 // Function to run top level callExpressions.
-// TODO(rachit): Make the run function check for stopped state.
 let runFunc = b.parseExpression(`
   function run(gen, res = { done: false }) {
-    if (gen !== undefined && typeof gen.next === 'function') {
+    setTimeout(_ => {
       res = gen.next();
       if (res.done) {
         return res.value
@@ -20,41 +16,36 @@ let runFunc = b.parseExpression(`
       else {
         return run(gen, res)
       }
-    } else {
-      return gen;
-    }
+    })
   }
 `);
-const { id, params, body } = runFunc;
-const runFuncName = runFunc.id;
+
+let runProg = b.parseExpression(`
+run($runProg())
+`)
 
 const program : VisitNode<t.Program> = {
-  exit : function (path: NodePath<t.Program>): void {
-    path.node.body.unshift(runFunc);
+  enter: function (path: NodePath<t.Program>): void {
+    const prog = path.node.body;
+    const func = t.functionDeclaration(
+      t.identifier('$runProg'), [], t.blockStatement(prog))
+    path.node.body = [func]
+  },
+  exit: function (path: NodePath<t.Program>): void {
+    path.node.body = [runFunc, ...path.node.body, runProg]
   },
 };
 
-const callExpression : VisitNode<CallExpression> = function (path: NodePath<CallExpression>): void {
-  if (path.node.dontTransform) return;
-  if (h.isConsoleLog(path.node.callee)) return;
+const callExpression: VisitNode<t.CallExpression> =
+  function (path: NodePath<t.CallExpression>): void {
+    if (h.isConsoleLog(path.node.callee)) return;
+    if (t.isYieldExpression(path.parent)) return;
 
-  if (t.isYieldExpression(path.parent)) return;
-  const funcParent = path.findParent(
-    p => p.isFunctionExpression() || p.isFunctionDeclaration());
-
-  // Inside another function.
-  if (funcParent !== null) {
     const yieldExpr = t.yieldExpression(path.node, true);
     path.replaceWith(yieldExpr);
-  } else {
-    path.node.dontTransform = true;
-    const runExpr = <CallExpression>t.callExpression(runFuncName, [path.node]);
-    runExpr.dontTransform = true;
-    path.replaceWith(runExpr);
-  }
-};
+  };
 
-const func : VisitNode<Function> = function (path: NodePath<Function>): void {
+const func: VisitNode<Function> = function (path: NodePath<Function>): void {
   // Add a dummy yield at the top of the function to force it to pause.
   const yieldExpr = t.yieldExpression(t.stringLiteral('dummy'), false);
   (<Function>path.node).body.body.unshift(t.expressionStatement(yieldExpr));

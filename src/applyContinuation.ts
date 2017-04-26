176dec04e9b456f293b6e2887ba50970f5cb2ad3
Rachit Nigam <rachit.nigam12@gmail.com>
2017-04-26 17:00:29 -0400

    Pass result to onDone in cps.

--- src/applyContinuation.ts
+++ src/applyContinuation.ts
@@ -4,13 +4,22 @@ import {NodePath, VisitNode, Visitor} from 'babel-traverse';
 import * as b from 'babylon';
 import * as t from 'babel-types';
 
+interface CallExpression extends t.CallExpression {
+  dontTransform: boolean;
+}
+
 const program : VisitNode<t.Program> = function (path: NodePath<t.Program>): void {
   const { body } = path.node;
 
   const k = path.scope.generateUidIdentifier('k');
   const cpsFunction = (<t.ExpressionStatement>body[0]).expression;
   const kArgs = <any>[k];
-  const kont = t.functionExpression(null, kArgs, t.blockStatement([t.returnStatement(k)]));
+  const onDoneCall =
+    <CallExpression>t.callExpression(t.identifier('onDone'), [k])
+  onDoneCall.dontTransform = true;
+  const kont =
+    t.functionExpression(null, kArgs, t.blockStatement([
+      t.returnStatement(onDoneCall)]))
   const cpsApply = t.callExpression(cpsFunction, [kont]);
 
   path.node.body = [t.expressionStatement(cpsApply)];

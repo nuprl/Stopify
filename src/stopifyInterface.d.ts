7765b7110f68f1a9444b7d2e5603147ef369ac09
Rachit Nigam <rachit.nigam12@gmail.com>
2017-04-27 13:35:14 -0400

    onStop passed to stop call instead of the object.

--- src/stopifyInterface.d.ts
+++ src/stopifyInterface.d.ts
@@ -1,11 +1,10 @@
 export interface Stoppable {
   run: (onDone: (any?) => any) => void;
-  stop: () => void;
+  stop: (onStop: () => any) => void;
   setInterval: (number) => void;
   transformed: string
 }
 
 export type stopify = (code: string,
     isStop: () => boolean,
-    onStop: () => any,
-    stop: () => void) => Stoppable;
+    onStop: () => any) => Stoppable;
--- src/stopifyYield.ts
+++ src/stopifyYield.ts
@@ -7,62 +7,64 @@ import { transform } from './helpers';
 
 class YieldStopify implements Stoppable {
   private original: string;
-  transformed: string;
   private isStop: () => boolean;
-  private onStop: () => any;
   private interval: number;
+  private stopTarget: () => void
+  transformed: string;
+  onStop: () => any;
 
-  constructor (code: string,
-    isStop: () => boolean,
-    onStop: () => any,
-    stop: () => void) {
-      this.original = code;
-      const plugins = [
-        [noArrows, desugarAndOr], [anf], [yieldPass]
-      ];
-      this.transformed = transform(code, plugins);
-      this.isStop = isStop;
-      this.onStop = onStop;
-      this.stop = stop;
-      this.interval = 10;
-    };
+  constructor (code: string, isStop: () => boolean, stop: () => void) {
+    this.original = code;
+    const plugins = [
+      [noArrows, desugarAndOr], [anf], [yieldPass]
+    ];
+    this.transformed = transform(code, plugins);
 
-    setInterval(that): void {
-      this.interval = that;
+    this.isStop = isStop;
+    const onStopDef = (function() { throw 'Execution terminated' })
+    this.onStop = onStopDef
+    this.stopTarget = stop;
+    this.interval = 10;
+  };
+
+  setInterval(that): void {
+    this.interval = that;
+  }
+
+  run(onDone: (any?) => any = x => x): void {
+    const $that = this;
+    const $yieldCounter = this.interval;
+    let $counter = 0;
+    const $runYield = function run(gen,
+      res = { done: false, value: undefined }) {
+      setTimeout(_ => {
+        if ($that.isStop()) {
+          $that.onStop();
+        }
+        res = gen.next();
+        if (res.done) {
+          return onDone(res.value);
+        }
+        else {
+          return run(gen, res);
+        }
+      }, 0)
     }
+    eval($that.transformed);
+  };
 
-    run(onDone: (any?) => any = x => x): void {
-      const that = this;
-      const $yieldCounter = this.interval;
-      let $counter = 0;
-      const $runYield = function run(gen,
-        res = { done: false, value: undefined }) {
-        setTimeout(_ => {
-          if (that.isStop()) {
-            that.onStop();
-          }
-          res = gen.next();
-          if (res.done) {
-            return onDone(res.value);
-          }
-          else {
-            return run(gen, res);
-          }
-        }, 0)
-      }
-      eval(that.transformed);
-    };
-
-    stop(): void {
-      return this.stop();
-    }
+  stop(onStop: () => any): void {
+    // TODO(rachit): Multiple calls to stop before stop is actually registered
+    // will override the first one. Make sure that this doesn't happen.
+    this.onStop = onStop;
+    this.stopTarget();
+  }
 };
 
 const yieldStopify : stopify = function (code: string,
   isStop: () => boolean,
-  onStop: () => any,
   stop: () => void): YieldStopify {
-    return new YieldStopify(code, isStop, onStop, stop);
+    return new YieldStopify(code, isStop, stop);
 }
 
 export {

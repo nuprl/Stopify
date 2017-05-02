7b915ce87e6c133aff106ef35959825f3e098ccb
Your Name <arjun@cs.umass.edu>
2017-05-02 14:49:57 -0400

    Web demo supports all transformations

---
+++ src/stopifySham.ts
@@ -0,0 +1,26 @@
+import { Stoppable, stopify } from './stopifyInterface';
+
+class Sham implements Stoppable {
+  transformed: string;
+
+  constructor(code: string) {
+    this.transformed = code;
+  }
+
+  run(onDone: (any?) => any) {
+    onDone(eval(this.transformed));
+  }
+
+  stop(onStop: () => any) {
+    onStop();
+  }
+  setInterval(n: number) {
+  }
+}
+
+export function shamStopify(
+  code: string,
+  isStop: () => boolean,
+  stop: () => void): Stoppable {
+  return new Sham(code);
+}

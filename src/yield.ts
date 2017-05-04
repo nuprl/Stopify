8a77b2ea6e03df02787622aefabfe431d32f0865
Rachit Nigam <rachit.nigam12@gmail.com>
2017-05-04 11:30:25 -0400

    Defined isGen on all generators.

--- src/yield.ts
+++ src/yield.ts
@@ -1,9 +1,8 @@
 import {NodePath, VisitNode, Visitor} from 'babel-traverse';
 import * as t from 'babel-types';
 import * as b from 'babylon';
+import generate from 'babel-generator';
 import * as h from './helpers';
-
-type Function = t.FunctionDeclaration|t.FunctionExpression;
 
 const runProg = t.expressionStatement(t.callExpression(
   t.identifier('$runYield'), [t.callExpression(t.identifier('$runProg'), [])]))
@@ -64,16 +63,41 @@ const loop: VisitNode<t.Loop> = function (path: NodePath<t.Loop>): void {
   }
 }
 
-const func: VisitNode<Function> = function (path: NodePath<Function>): void {
+const funcd: VisitNode<t.FunctionDeclaration> =
+  function (path: NodePath<t.FunctionDeclaration>): void {
   // Add a dummy yield at the top of the function to force it to pause.
-  (<Function>path.node).body.body.unshift(ifYield);
+  path.node.body.body.unshift(ifYield);
   path.node.generator = true;
+
+  // Set isGen property on the function.
+  const assign = t.assignmentExpression('=',
+    t.memberExpression(path.node.id, t.identifier('$isGen')),
+    t.booleanLiteral(true))
+  path.insertAfter(t.expressionStatement(assign))
 };
+
+const funce: VisitNode<t.FunctionExpression> =
+  function (path: NodePath<t.FunctionExpression>): void {
+    // Set isGen property on the function.
+    const decl = path.parent;
+    if (!t.isVariableDeclarator(decl)) {
+      throw new Error(`Parent of function expression was ${decl.type}`)
+    } else {
+      path.node.body.body.unshift(ifYield);
+      path.node.generator = true;
+
+      // Decl will always be a variable so casting it to expression is fine.
+      const assign = t.assignmentExpression('=',
+        t.memberExpression(<t.Expression>decl.id, t.identifier('$isGen')),
+        t.booleanLiteral(true))
+      path.getStatementParent().insertAfter(t.expressionStatement(assign))
+    }
+}
 
 const yieldVisitor: Visitor = {
   Program: program,
-  FunctionDeclaration: func,
-  FunctionExpression: func,
+  FunctionDeclaration: funcd,
+  FunctionExpression: funce,
   CallExpression: callExpression,
   "Loop": loop,
 }

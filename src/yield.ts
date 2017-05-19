9439e7d124c244ec98b2400f1dd28a19778eb0f7
Rachit Nigam <rachit.nigam12@gmail.com>
2017-05-19 13:04:00 -0400

    Start yield transform rewrite. factorial works.

--- src/yield.ts
+++ src/yield.ts
@@ -13,7 +13,12 @@ function transformed<N>(n: Transformed<N>): Transformed<N> {
 }
 
 const runProg = t.expressionStatement(t.callExpression(
-  t.identifier('$runYield'), [t.callExpression(t.identifier('$runProg'), [])]))
+  t.identifier('$runYield'),
+  [t.callExpression(
+    t.memberExpression(t.identifier('$runProg'), t.identifier('__generator__')),
+    []
+  )]
+))
 
 const ifYield = t.ifStatement(
   t.binaryExpression('===',
@@ -62,10 +67,15 @@ const callExpression: VisitNode<Transformed<t.CallExpression>> =
     if(exp.isTransformed) return
     else exp.isTransformed = true;
 
+    const { callee, arguments: args } = path.node;
+
     const cond = t.conditionalExpression(
       t.memberExpression(path.node.callee, t.identifier('$isGen')),
-      t.yieldExpression(path.node, true),
-      path.node)
+      t.yieldExpression(transformed(t.callExpression(
+        t.memberExpression(callee, t.identifier('__generator__')), args
+      )), true),
+      path.node
+    )
     path.replaceWith(cond);
   };
 
@@ -79,33 +89,44 @@ const loop: VisitNode<t.Loop> = function (path: NodePath<t.Loop>): void {
 
 const funcd: VisitNode<t.FunctionDeclaration> =
   function (path: NodePath<t.FunctionDeclaration>): void {
-  // Add a dummy yield at the top of the function to force it to pause.
-  path.node.body.body.unshift(ifYield);
-  path.node.generator = true;
+    // Create __generator__ property for function
+    const { params, id }  = path.node
+    // Make a copy of the body.
+    const body = path.node.body.body.slice(0)
+    body.unshift(ifYield);
+    const genFunc = t.functionExpression(undefined, params, t.blockStatement(body), true, false)
+    const assignGen = t.assignmentExpression('=',
+      t.memberExpression(id, t.identifier('__generator__')), genFunc
+    )
+    path.insertAfter(t.expressionStatement(assignGen))
 
-  // Set isGen property on the function.
-  const assign = t.assignmentExpression('=',
-    t.memberExpression(path.node.id, t.identifier('$isGen')),
-    t.booleanLiteral(true));
-  const callAssign = t.assignmentExpression('=',
-    t.memberExpression(
-      t.memberExpression(path.node.id, t.identifier('call')),
-      t.identifier('$isGen')),
-    t.booleanLiteral(true)
-  )
-  const applyAssign = t.assignmentExpression('=',
-    t.memberExpression(
-      t.memberExpression(path.node.id, t.identifier('apply')),
-      t.identifier('$isGen')),
-    t.booleanLiteral(true)
-  )
+    // Change body of function to throw statement
+    const throwString = `Generator function ${id.name} called`
+    path.node.body.body = [t.throwStatement(t.stringLiteral(throwString))]
 
-  path.insertAfter(t.expressionStatement(assign))
-  path.insertAfter(t.expressionStatement(callAssign))
-  path.insertAfter(t.expressionStatement(applyAssign))
+    // Set isGen property on the function.
+    const assign = t.assignmentExpression('=',
+      t.memberExpression(path.node.id, t.identifier('$isGen')),
+      t.booleanLiteral(true));
+    const callAssign = t.assignmentExpression('=',
+      t.memberExpression(
+        t.memberExpression(path.node.id, t.identifier('call')),
+        t.identifier('$isGen')),
+      t.booleanLiteral(true)
+    )
+    const applyAssign = t.assignmentExpression('=',
+      t.memberExpression(
+        t.memberExpression(path.node.id, t.identifier('apply')),
+        t.identifier('$isGen')),
+      t.booleanLiteral(true)
+    )
+
+    path.insertAfter(t.expressionStatement(assign))
+    path.insertAfter(t.expressionStatement(callAssign))
+    path.insertAfter(t.expressionStatement(applyAssign))
 };
 
-const funce: VisitNode<t.FunctionExpression> =
+/*const funce: VisitNode<t.FunctionExpression> =
   function (path: NodePath<t.FunctionExpression>): void {
     // Set isGen property on the function.
     const decl = path.parent;
@@ -137,7 +158,7 @@ const funce: VisitNode<t.FunctionExpression> =
       path.getStatementParent().insertAfter(t.expressionStatement(callAssign))
       path.getStatementParent().insertAfter(t.expressionStatement(applyAssign))
     }
-};
+};*/
 
 const newVisit: VisitNode<Transformed<t.NewExpression>> =
   function (path: NodePath<Transformed<t.NewExpression>>): void {
@@ -172,7 +193,7 @@ const newVisit: VisitNode<Transformed<t.NewExpression>> =
 
 const yieldVisitor: Visitor = {
   FunctionDeclaration: funcd,
-  FunctionExpression: funce,
+  //FunctionExpression: funce,
   CallExpression: callExpression,
   "Loop": loop,
   NewExpression: newVisit,

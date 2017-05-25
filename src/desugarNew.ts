1da62964dfdd6b2edc784ed475a11477ba33d111
Sam Baxter <sbaxter@cs.umass.edu>
2017-05-25 09:18:31 -0400

    Build AST for handleNew manually

--- src/desugarNew.ts
+++ src/desugarNew.ts
@@ -6,43 +6,57 @@
 
 import {NodePath, VisitNode, Visitor} from 'babel-traverse';
 import * as t from 'babel-types';
-import { parseExpression } from 'babylon';
-import { letExpression, transformed, Tag, OptimizeMark } from './helpers';
+import { letExpression, transformed, OptimizeMark } from './helpers';
 
-const handleNewFunc = letExpression(
-  t.identifier('$handleNew'),
-  transformed(parseExpression(`
-    function (constr, ...args) {
-      const $knownBuiltIns = [
-        WeakMap,
-        Map,
-        Set,
-        WeakSet,
-        String,
-        Number,
-        Function,
-        Object,
-        Array,
-        Date,
-        RegExp,
-        Error
-      ]
+const newFuncName: t.Identifier = t.identifier('$handleNew');
+const knownsId: t.Identifier = t.identifier('$knownBuiltIns');
 
-      if($knownBuiltIns.includes(constr)) {
-        // Don't transform this new
-        return new constr(...args)
-      } else {
-        // This should be transformed.
-        const a = Object.create(constr.prototype);
-        constr.apply(a, args)
-        return a;
-      }
-    }
-`, [])))
+function makeIdList(ids: string[]): t.Identifier[] {
+  return ids.map(id => t.identifier(id));
+}
+
+const knowns = letExpression(knownsId, t.arrayExpression(makeIdList(
+  ['WeakMap',
+    'Map',
+    'Set',
+    'WeakSet',
+    'String',
+    'Number',
+    'Function',
+    'Object',
+    'Array',
+    'Date',
+    'RegExp',
+    'Error'])));
+
+const constr: t.Identifier = t.identifier('constr');
+const restArgs: t.RestElement = t.restElement(t.identifier('args'));
+const spreadArgs: t.SpreadElement = t.spreadElement(t.identifier('args'));
+
+const handleNewFunc = t.functionDeclaration(
+  newFuncName,
+  [constr, restArgs],
+  t.blockStatement([
+    t.ifStatement(
+      t.callExpression(t.memberExpression(knownsId, t.identifier('includes')),
+        [constr]),
+      t.blockStatement([t.returnStatement(t.newExpression(constr, [spreadArgs]))]),
+      t.blockStatement([
+        letExpression(t.identifier('a'),
+          t.callExpression(t.memberExpression(t.identifier('Object'),
+            t.identifier('create')),
+          [t.memberExpression(constr, t.identifier('prototype'))])),
+        t.expressionStatement(t.callExpression(t.memberExpression(constr,
+          t.identifier('apply')), [t.identifier('a'), t.identifier('args')])),
+        t.returnStatement(t.identifier('a'))
+      ]),
+  )]
+));
 
 const program: VisitNode<t.Program> = {
   exit(path: NodePath<t.Program>) {
-    path.node.body.unshift(handleNewFunc)
+    path.node.body.unshift(handleNewFunc);
+    path.node.body.unshift(knowns);
   }
 }
 

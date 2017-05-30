fdadc2cfe93bd5ffe92753ecdab91eab023e04a3
Sam Baxter <sbaxter@cs.umass.edu>
2017-05-30 13:50:46 -0400

    Visit functions on exit in transformMarked and skip children

--- src/transformMarked.ts
+++ src/transformMarked.ts
@@ -40,22 +40,25 @@ const program : VisitNode<t.Program> = {
   },
 };
 
-const funcd: VisitNode<h.Transformed<t.FunctionDeclaration>> =
-  function (path: NodePath<h.Transformed<t.FunctionDeclaration>>) {
+const funcd: VisitNode<h.Transformed<t.FunctionDeclaration>> = {
+  exit(path: NodePath<h.Transformed<t.FunctionDeclaration>>) {
     if(path.node.isTransformed) {
       const markCall = h.directApply(t.callExpression(markFuncName, [path.node.id]));
       path.insertAfter(markCall)
     }
+  }
 }
 
 
-const funce: VisitNode<h.Transformed<t.FunctionExpression>> =
-  function (path: NodePath<h.Transformed<t.FunctionExpression>>) {
+const funce: VisitNode<h.Transformed<t.FunctionExpression>> = {
+  exit(path: NodePath<h.Transformed<t.FunctionExpression>>) {
     if(path.node.isTransformed) {
       path.node.isTransformed = false;
       path.replaceWith(h.directApply(t.callExpression(markFuncName, [path.node])));
+      path.skip();
     }
   }
+}
 
 const visitor = {
   Program: program,

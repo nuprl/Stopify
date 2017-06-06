5b74058fa215c33fecd96655ae9c64a8fb00393e
Sam Baxter <sbaxter@cs.umass.edu>
2017-06-06 09:58:28 -0400

    Added CPS Monad interface to cpsSyntax

--- src/cpsSyntax.ts
+++ src/cpsSyntax.ts
@@ -17,6 +17,26 @@ type unop = "-" | "+" | "!" | "~" | "typeof" | "void" | "delete";
 
 type kind = 'const' | 'var' | 'let' | undefined;
 
+class CPS<A,R> {
+  constructor(private m: (k: (a: A) => R) => R) {}
+
+  bind<B>(f: (a: A) => CPS<B,R>): CPS<B,R> {
+    return new CPS((k: ((b: B) => R)) => this.m((a: A) => f(a).m(k)));
+  }
+
+  map<B>(f: (a: A) => B): CPS<B,R> {
+    return new CPS((k: (b: B) => R) => this.m((a: A) => k(f(a))));
+  }
+
+  apply(f: (a: A) => R): R {
+    return this.m(f);
+  }
+};
+
+function ret<A,R>(a: A): CPS<A,R> {
+  return new CPS((k: ((a: A) => R)) => k(a));
+};
+
 const nullLoc : any  = {
   start: {
     line: null,

c158127dd8d8829676764719715e9b253065c3aa
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-07 19:37:12 -0400

    Add runner harness. Not generic over JS engine yet

--- benchmarks/Makefile
+++ benchmarks/Makefile
@@ -2,23 +2,37 @@
 DIRS = $(shell find . -type d -maxdepth 1 -mindepth 1)
 BUILD = $(DIRS:%=%/js-build)
 CLEANDIRS = $(DIRS:%=%-clean)
+
 STOPIFYMK = "./stopify-Makefile"
+RUNNER = "./runner.sh"
+RUNNERMK = "./runner-Makefile"
 
 all: build stopify
 
-build: $(BUILD) $(BUILD:%=%/stopify-makefile)
 # Compile all source language programs to javascript.
+build: $(BUILD) $(BUILD:%=%/stopify-Makefile)
 %/js-build: %
 	$(MAKE) --ignore-errors -C $^
 
 # Add makefiles for compile file using stopify
-%/js-build/stopify-makefile: %/js-build/
+%/js-build/stopify-Makefile: %/js-build/
 	cp $(STOPIFYMK) $^
 
 # Compile all JS source files with stopify
 stopify: $(BUILD:%=%/stopify)
-%/js-build/stopify:  %/js-build/stopify-makefile
+%/js-build/stopify:  %/js-build/stopify-Makefile
 	$(MAKE) -C $(patsubst %/stopify, %, $@) -f $(STOPIFYMK)
+
+# Rules for running the benchmarking harness.
+run: all $(BUILD:%=%/runner.sh) $(BUILD:%=%/runner-Makefile) \
+	$(BUILD:%=%/run)
+
+%/js-build/runner.sh: %/js-build
+	cp $(RUNNER) $^
+%/js-build/runner-Makefile: %/js-build
+	cp $(RUNNERMK) $^
+%/js-build/run:
+	$(MAKE) -C $(patsubst %/run, %, $@) -f $(RUNNERMK)
 
 # Rules for cleanup
 clean: $(CLEANDIRS)
---
+++ benchmarks/runner.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# First argument is the path to the JS engine.
+# Second argument is the name of the JS engine.
+# Third argument is the name of the directory containing the benchmarks.
+# Creates the file <engine name>-<dirname>-times.csv
+if [ "$(uname)" == "Darwin" ]; then
+  TIME=`which gtime`
+elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  TIME="/usr/bin/time"
+fi
+TIMEFLAGS='--append --format "%E"'
+
+function cleanup {
+rm -f tmp.js
+}
+
+trap cleanup EXIT
+
+if [[ $# -lt 3 ]]; then
+  echo 'Not enough arguments provided'
+  exit 1
+fi
+
+if [[ ! -f $1 ]]; then
+  echo "Could not find $1. It should refer to a javascript engine"
+  exit 1
+fi
+
+ENGINE=$1
+NAME=$2
+BENCHDIR=$3
+
+# Test the JS engine.
+echo '1 + 2' > tmp.js
+"$ENGINE" tmp.js
+if [[ $? -ne 0 ]]; then
+  echo "Failed to execute simple 1 + 2 with $ENGINE."
+  exit 1
+fi
+rm tmp.js
+
+# Cleanup name of the directory.
+IFS='/' read -ra a <<< "$BENCHDIR"
+LAST=${a[${#a[@]}-1]}
+
+# Make the log file
+LOG="$NAME-$LAST-times.csv"
+echo "" > $LOG
+
+# Log time taken to run each file.
+for i in `ls $BENCHDIR`; do
+  echo -n "$i," >> $LOG
+  $TIME --output=$LOG $TIMEFLAGS  "$ENGINE" $BENCHDIR/$i
+done

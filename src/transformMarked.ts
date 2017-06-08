f1be9ea5692666058d4206f60366c9f2b1f14ef3
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-08 14:57:53 -0400

    Formatting.

--- src/transformMarked.ts
+++ src/transformMarked.ts
@@ -7,18 +7,19 @@ const markFuncName = t.identifier('$mark_func')
 const funcd: VisitNode<h.Transformed<t.FunctionDeclaration>> = {
   exit(path: NodePath<h.Transformed<t.FunctionDeclaration>>) {
     if(path.node.isTransformed) {
-      const markCall = h.directApply(t.callExpression(markFuncName, [path.node.id]));
+      const markCall = h.directApply(
+        t.callExpression(markFuncName, [path.node.id]));
       path.insertAfter(markCall)
     }
   }
 }
 
-
 const funce: VisitNode<h.Transformed<t.FunctionExpression>> = {
   exit(path: NodePath<h.Transformed<t.FunctionExpression>>) {
     if(path.node.isTransformed) {
       path.node.isTransformed = false;
-      path.replaceWith(h.directApply(t.callExpression(markFuncName, [path.node])));
+      path.replaceWith(
+        h.directApply(t.callExpression(markFuncName, [path.node])));
       path.skip();
     }
   }

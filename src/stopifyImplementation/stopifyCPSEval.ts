b38c8fdef9fd28de8df05613921f4ea5a568d621
Sam Baxter <sbaxter@cs.umass.edu>
2017-06-08 14:36:57 -0400

    Paste cps runtime helpers as a string to the transformed output

--- src/stopifyImplementation/stopifyCPSEval.ts
+++ src/stopifyImplementation/stopifyCPSEval.ts
@@ -25,52 +25,6 @@ import * as transformMarked from '../transformMarked';
 // Helpers
 import {transform} from '../helpers';
 
-type MaybeBound = {
-  (...args: any[]): any,
-  $isTransformed?: boolean,
-}
-
-function onError(arg?: any) {
-  throw new Error(`Unexpected error: ${arg}`);
-};
-
-function applyWithK(f: MaybeBound, k: any, ek: any, ...args: any[]) {
-  if (f.$isTransformed) {
-    return f(k, ek, ...args);
-  } else {
-    try {
-      return k(f(...args));
-    } catch (e) {
-      return ek(e);
-    }
-  }
-};
-
-function call_applyWithK(f: MaybeBound, k: any, ek: any, ...args: any[]) {
-  const [hd, ...tail] = args;
-  if (f.$isTransformed) {
-    return f.call(hd, k, ek, ...tail);
-  } else {
-    try {
-      return k(f.call(hd, ...tail));
-    } catch (e) {
-      return ek(e);
-    }
-  }
-};
-
-function apply_applyWithK(f: MaybeBound, k: any, ek: any, thisArg: any, args: any[]) {
-  if (f.$isTransformed) {
-    return f.apply(thisArg, [k, ek, ...args]);
-  } else {
-    try {
-      return k(f.apply(thisArg, args));
-    } catch (e) {
-      return ek(e);
-    }
-  }
-};
-
 class CPSStopify implements Stoppable {
   private original: string;
   transformed: string;
@@ -87,9 +41,82 @@ class CPSStopify implements Stoppable {
         [desugarFunctionDecl, liftVar, noArrows, desugarLoop, desugarLabel, desugarNew],
         [desugarSwitch, desugarWhileToFunc],
         [makeBlockStmt, addKArg],
-        [cps, applyStop, transformMarked, cpsRuntime],
+        [cps, applyStop, transformMarked],
       ];
-      this.transformed = transform(code, plugins);
+    const cpsHelpers = `
+"use strict";
+
+let that = this;
+let counter = that.interval;
+
+function $mark_func(f) {
+  f.$isTransformed = true;
+  f.call = f.call.bind(f);
+  f.call.$isTransformed = true;
+  f.apply = f.apply.bind(f);
+  f.apply.$isTransformed = true;
+  Object.defineProperty(f, "length", {
+    configurable: true,
+    value: f.length - 2
+  });
+  return f;
+}
+
+function onError(arg) {
+  throw new Error('Unexpected error: ' + arg);
+}
+
+function applyWithK(f, k, ek, ...args) {
+  if (f.$isTransformed) return f(k, ek, ...args);else try {
+    return k(f(...args));
+  } catch (e) {
+    return ek(e);
+  }
+}
+
+function call_applyWithK(f, k, ek, ...args) {
+  const [hd, ...tail] = args;
+  if (f.$isTransformed) return f.call(hd, k, ek, ...tail);else try {
+    return k(f.call(hd, ...tail));
+  } catch (e) {
+    return ek(e);
+  }
+}
+
+function apply_applyWithK(f, k, ek, thisArg, args) {
+  if (f.$isTransformed) return f.apply(thisArg, [k, ek, ...args]);else try {
+    return k(f.apply(thisArg, args));
+  } catch (e) {
+    return ek(e);
+  }
+}
+
+function apply_helper(how) {
+  return function (f, k, ek, ...args) {
+    if (counter-- === 0) {
+      counter = that.interval;
+      setTimeout(_ => {
+        if (that.isStop()) that.onStop();else return how(f, k, ek, ...args);
+      }, 0);
+    } else return how(f, k, ek, ...args);
+  };
+}
+
+const admin_apply = apply_helper(function (f, ...args) {
+  return f(...args);
+});
+const apply = apply_helper(function (f, k, ek, ...args) {
+  return applyWithK(f, k, ek, ...args);
+});
+const call_apply = apply_helper(function (f, k, ek, ...args) {
+  return call_applyWithK(f, k, ek, ...args);
+});
+const apply_apply = apply_helper(function (f, k, ek, thisArg, args) {
+  return apply_applyWithK(f, k, ek, thisArg, args);
+});
+
+    `;
+      this.transformed = cpsHelpers + transform(code, plugins);
 
       if(this.transformed.length < code.length) {
         throw new Error('Transformed code is smaller than original code')
@@ -101,43 +128,7 @@ class CPSStopify implements Stoppable {
     };
 
   run(onDone: (arg?: any) => any): void {
-    'use strict';
-    const that = this;
-    let counter = that.interval;
-    let apply_helper = function (how: any) {
-      return function (f: MaybeBound, k: any, ek: any, ...args: any[]) {
-        if (counter-- === 0) {
-          counter = that.interval;
-          setTimeout(_ => {
-            if (that.isStop()) {
-              that.onStop();
-            } else {
-              return how(f, k, ek, ...args);
-            }
-          }, 0);
-        } else {
-          return how(f, k, ek, ...args);
-        }
-      };
-    };
-    let admin_apply = apply_helper(function (f: MaybeBound, ...args: any[]) {
-      return f(...args);
-    });
-    let apply = apply_helper(function (f: MaybeBound, k: any, ek: any, ...args: any[]) {
-      return applyWithK(f, k, ek, ...args);
-    });
-    let call_apply = apply_helper(function (f: MaybeBound, k: any, ek: any, ...args: any[]) {
-      return call_applyWithK(f, k, ek, ...args);
-    });
-    let apply_apply = apply_helper(function (f: MaybeBound,
-      k: any,
-      ek: any,
-      thisArg: any,
-      args: any[]) {
-      return apply_applyWithK(f, k, ek, thisArg, args);
-    });
-
-    eval(that.transformed);
+    eval(this.transformed);
   };
 
   stop(onStop: () => any): void {

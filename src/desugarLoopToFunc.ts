965c1a48513e0ec86b0ebbafb784cc907180bc44
Sam Baxter <sbaxter@cs.umass.edu>
2017-06-12 19:03:12 -0400

    Turn while loops into functions on exit instead of enter

--- src/desugarLoopToFunc.ts
+++ src/desugarLoopToFunc.ts
@@ -5,24 +5,26 @@ import * as h from './helpers';
 module.exports = function() {
   return {
     visitor: {
-      WhileStatement: function WhileStatement(path: NodePath<t.WhileStatement>) {
-        const { test, body } = path.node
+      WhileStatement: {
+        exit(path: NodePath<t.WhileStatement>) {
+          const { test, body } = path.node
 
-        // Name the function representing the while loop.
-        const fName = path.scope.generateUidIdentifier('while');
+          // Name the function representing the while loop.
+          const fName = path.scope.generateUidIdentifier('while');
 
-        // Create the body for the function.
-        const fBody = h.flatBodyStatement([
-          t.ifStatement(test, h.flatBodyStatement([body,
-            t.returnStatement(t.callExpression(t.memberExpression(fName,
-              t.identifier('call')), [t.thisExpression()]))]))]);
-        // Create the function representing the while loop.
-        const fExpr = t.functionExpression(fName, [], fBody);
-        const fDecl = h.letExpression(fName, fExpr);
-        path.replaceWith(h.flatBodyStatement([fDecl, t.expressionStatement(
-          t.callExpression(t.memberExpression(fName,
-            t.identifier('call')), [t.thisExpression()]))]));
+          // Create the body for the function.
+          const fBody = h.flatBodyStatement([
+            t.ifStatement(test, h.flatBodyStatement([body,
+              t.returnStatement(t.callExpression(t.memberExpression(fName,
+                t.identifier('call')), [t.thisExpression()]))]))]);
+          // Create the function representing the while loop.
+          const fExpr = t.functionExpression(fName, [], fBody);
+          const fDecl = h.letExpression(fName, fExpr);
+          path.replaceWith(h.flatBodyStatement([fDecl, t.expressionStatement(
+            t.callExpression(t.memberExpression(fName,
+              t.identifier('call')), [t.thisExpression()]))]));
+        },
       },
-    },
+    }
   }
 }

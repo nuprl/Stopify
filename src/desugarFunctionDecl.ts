79326a8b42555597e93fd49358c6f13802e322d5
Sam Baxter <sbaxter@cs.umass.edu>
2017-06-12 18:46:40 -0400

    Desugar FunctionDeclarations on enter instead of exit

--- src/desugarFunctionDecl.ts
+++ src/desugarFunctionDecl.ts
@@ -3,12 +3,10 @@ import * as t from 'babel-types';
 import {letExpression} from './helpers';
 
 const functionDecl: Visitor = {
-  FunctionDeclaration: {
-    exit(path: NodePath<t.FunctionDeclaration>) {
-      const { id, params, body } = path.node;
-      path.replaceWith(letExpression(id,
-        t.functionExpression(id, params, body), 'var'));
-    }
+  FunctionDeclaration: function (path: NodePath<t.FunctionDeclaration>) {
+    const { id, params, body } = path.node;
+    path.replaceWith(letExpression(id,
+      t.functionExpression(id, params, body), 'var'));
   }
 };
 

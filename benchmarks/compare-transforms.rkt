e6f044a4eadbeb5d153a71cbeac51ff09bb41713
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-18 15:24:57 -0400

    Add typed racket scripts.

--- benchmarks/compare-transforms.rkt
+++ benchmarks/compare-transforms.rkt
@@ -1,36 +1,48 @@
-#lang racket/base
+#lang typed/racket/base
 
 ;; This script assumes that programs given are from the same JS engine.
 (require racket/list)
+(require typed/racket/unsafe)
 (require plot/no-gui)
-(require slideshow/pict)
 (require "plot-helpers.rkt")
 
-;; This assumes that the data in both the lists is sorted according to
-;; column 1.
+; This assumes that the data in both the lists is sorted according to
+; column 1.
+(: make-normalization-function (-> String
+                                   (-> (Listof (Vector String Real))
+                                       (Listof (Vector String Real)))))
 (define (make-normalization-function basefile)
   (let ([ base-list (csvfile->list/proc basefile v2-to-number)])
-    (lambda (fs)
-      (for/list ( [data fs] [ base base-list ] )
-        (list (vector-ref data 0)
-              (/ (vector-ref data 1) (vector-ref base 1)))))))
+    (lambda ([fs : (Listof (Vector String Real))])
+      (for/list ([data fs] [ base base-list ])
+        (vector
+          (vector-ref data 0)
+          (/ (vector-ref data 1) (vector-ref base 1)))))))
 
-;; Given a list of CSV names, plot a side-by-side graph.
-;: ASSUMPTIONS:IN EACH FILE Column 1 is the name of files, Column 2 is the time.
-;; Outputs a CSV with X-axis as names of files and Y-axis as time in seconds.
-;; This is meant to compare the transforms.
-;; (Listof String) * String * (String * String -> String * String) -> void
+; Given a list of CSV names, plot a side-by-side graph.
+; This is meant to compare the transforms.
+(: make-compare-plot (->
+                       (Listof String)
+                       String
+                       (-> (Listof (Vector String Real))
+                           (Listof (Vector String Real)))
+                       pict))
 (define (make-compare-plot files title normalize)
+  (: l Nonnegative-Real)
   (define l (length files))
+
+  (: x-min-val Integer)
   (define x-min-val -1)
+
+  (: make-hist (-> String renderer2d))
   (define (make-hist file)
     (set! x-min-val (+ x-min-val 1))
     (discrete-histogram
-      (normalize (csvfile->list/proc file v2-to-number))
+      (to-plot-type (normalize (csvfile->list/proc file v2-to-number)))
       #:skip (+ l 1) #:x-min x-min-val
       #:label (get-name file)
       #:color (+ x-min-val 1) #:line-color (+ x-min-val 1)))
-  (plot-jpeg-quality 100)
+
   (parameterize ([plot-x-tick-label-anchor 'top-right]
                  [plot-x-tick-label-angle 30])
     (inset (plot-pict (map make-hist files)
@@ -39,33 +51,44 @@
                       #:legend-anchor 'top-right
                       #:title title) 15)))
 
-;; Extract number from the benchmark name format
+;; Extract Number from the benchmark name format
+(: extract-num (-> String Real))
 (define (extract-num f)
   (let ([res (regexp-match #px"\\d+" f) ])
-    (if res (string->number (car res)) -1)))
+    (if (not (boolean? res)) (safe-string->number (first res)) -1)))
 
-;; Group names according to the numbers in the file name.
+; Group names according to the Numbers in the file name.
+(: group-names (-> (Listof String) (Listof (Listof String))))
 (define (group-names files)
   (group-by extract-num files))
 
-(let ([ args (vector->list (current-command-line-arguments)) ])
+(let ([ args : (Listof String)
+             (vector->list (current-command-line-arguments)) ])
+
+  (: remove-base (-> (Listof (Listof String)) (Listof (Listof String))))
   (define (remove-base fss)
-    (filter (lambda (fs) (> (extract-num (car fs)) 0)) fss))
+    (filter (lambda ([fs : (Listof String)]) (> (extract-num (car fs)) 0)) fss))
+
+  (: sort-with-interval (-> (Listof (Listof String)) (Listof (Listof String))))
   (define (sort-with-interval fss)
     (sort fss
-          (lambda (xs ys)
+          (lambda ([xs : (Listof String)] [ys : (Listof String)])
             (< (extract-num (car xs)) (extract-num (car ys))))))
   (if (< (length args) 2)
     (raise
       (format "script requires at least 2 arguments, given: ~a" (length args)))
-    (let* ([ output-file (car args) ]
-           [ files (cdr args) ]
+    (let* ([ output-file : String (car args) ]
+           [ files : (Listof String) (cdr args) ]
            [ groups (sort-with-interval (remove-base (group-names files))) ]
-           [ base-file (findf (lambda (f) (regexp-match? #rx"base" f)) files) ]
-           [ _tmp (if base-file #t (raise "No basefile found")) ]
+           [ base-file : String
+                       (extract-val
+                         (findf
+                           (lambda ([f : String])
+                             (regexp-match? #rx"-base-" f)) files)
+                         #:err "No base file present in list of files") ]
            [ normalize (make-normalization-function base-file)]
            [ plots (map
-                     (lambda (fs)
+                     (lambda ([fs : (Listof String)])
                        (make-compare-plot
                          (cons base-file fs)
                          (number->string (extract-num (car fs)))
--- benchmarks/plot-helpers.rkt
+++ benchmarks/plot-helpers.rkt
@@ -1,46 +1,96 @@
-#lang racket/base
+#lang typed/racket/base
 
+(require typed/racket/unsafe)
 (require racket/list)
-(require csv-reading)
-(require file/convertible)
-(require slideshow/pict)
+(require plot/no-gui)
+(require/typed csv-reading
+               [csv-map (All (a) (->
+                                   (-> (Listof String) a)
+                                   (U String Input-Port)
+                                   (Listof a)))])
+(require/typed slideshow/pict
+               [#:struct pict ([draw : Any]
+                               [width : Real]
+                               [height : Real]
+                               [ascent : Real]
+                               [descent : Real]
+                               [children : (Listof Any)]
+                               [panbox : (U Boolean Any)]
+                               [last : Any])]
+               [vc-append (-> Integer pict pict pict)])
+(unsafe-require/typed slideshow/pict
+               [inset (-> Any Real pict)])
+(require/typed file/convertible
+               [convert (-> pict Symbol Bytes)])
 
 (provide
   handle-time-output csvfile->list/proc get-name draw-plot join-plots
-  v2-to-number pr)
+  v2-to-number pr safe-string->number extract-val to-plot-type
+  ; Export definitons from slideshow
+  pict inset)
 
+(: pr (All (A) (-> A A)))
 (define (pr v)
-    (printf "~a\n" v)
-      v)
+  (printf "~a\n" v)
+  v)
+
+; Typed version of string->number
+(: safe-string->number (-> String Real))
+(define (safe-string->number str)
+  (real-part (extract-val (string->number str)
+               #:err (format "~a str is not a number" str))))
+
+(: extract-val (All (A) (-> (U A False) (#:err String) A)))
+(define (extract-val val #:err [err-string "failed to extract-val"])
+  (if (boolean? val) (raise err-string) val))
+
 ;; Interpret the output from unix time with --format %E.  MM:SS
 ;; Result in seconds
+(: time->number (-> String Real))
 (define (time->number t)
-  (let* ([matches (regexp-match #px"(\\d+):([0-9.]+)" t)]
-         [mins (string->number (cadr matches))]
-         [secs (string->number (caddr matches))])
+  (let* ([matches
+           (extract-val
+             (regexp-match #px"(\\d+):([0-9.]+)" t)
+             #:err (format "~a is not in the correct time format" t))]
+         [mins (safe-string->number (extract-val (second matches)))]
+         [secs (safe-string->number (extract-val (third matches)))])
     (+ (* 60 mins) secs)))
 
 ;; In addition to the format specified in time->number, the output can be
 ;; -1 for programs that failed to run.
+(: handle-time-output (-> String Real))
 (define (handle-time-output t)
-  (if (string->number t) (string->number t) (time->number t)))
+  (if (string->number t) (safe-string->number t) (time->number t)))
 
-;; Convert a CSV file into form usable by plot while processing each row using
-;; `proc`. The resulting list is sorted according to column 1
-;; (ListOf String -> ListOf String) -> ListOf #(String Number)
+(: csvfile->list/proc
+   (All (A) (-> String
+                (-> (Vector String String) (Vector String A))
+                (Listof (Vector String A)))))
 (define (csvfile->list/proc filename proc)
-  (sort (csv-map (compose list->vector proc) (open-input-file filename))
-        (lambda (x y) (string<? (vector-ref x 0) (vector-ref y 0)))))
+  (: map-proc (All (A)
+                   (-> (-> (Vector String String) (Vector String A))
+                       (-> (Listof String) (Vector String A)))))
+  (define (map-proc proc)
+    (lambda ([fs : (Listof String)])
+      (if (not (= (length fs) 2))
+        (raise (format "~a: ~s is an invalid field" filename fs))
+        (proc (vector (first fs) (second fs))))))
+  (sort (csv-map (map-proc proc) (open-input-file filename))
+        (lambda ([x : (Vectorof String)]
+                 [y : (Vectorof String)])
+          (string<? (vector-ref x 0) (vector-ref y 0)))))
 
 ;; This relies on the name of the file being in the format specified by the
 ;; Makefile runner.
+(: get-name (-> String String))
 (define (get-name f)
-  (let ([res (regexp-match #px"[^-]*-(.*)-times\\.csv" f)])
-    (if (not res)
-      (raise (format "~a is not in the required format" f))
-      (cadr res))))
+  (let ([res (extract-val
+               (regexp-match #px"[^-]*-(.*)-times\\.csv" f)
+               #:err (format "~a is not in the required format" f))])
+    (extract-val (second res))))
 
 ;; Draw picture to the specified PDF file.
+(: draw-plot (-> String pict Void))
 (define (draw-plot filename pict)
   (define to-write (convert pict 'pdf-bytes))
   (define out-file (open-output-file filename
@@ -50,8 +100,20 @@
   (close-output-port out-file))
 
 ;; Join plots together
+(: join-plots (-> (Listof pict) pict))
 (define (join-plots plots)
-  (foldl (lambda (x y) (vc-append 30 x y)) (car plots) (cdr plots)))
+  (foldl (lambda ([x : pict]
+                  [y : pict]) (vc-append 30 x y)) (car plots) (cdr plots)))
 
-(define (v2-to-number c)
-    (list (car c) (handle-time-output (cadr c))))
+(: v2-to-number (-> (Vector String String) (Vector String Real)))
+(define (v2-to-number entry)
+  (vector (vector-ref entry 0)
+          (handle-time-output (vector-ref entry 1))))
+
+; Annotate the types to be accepted by plot functions.
+(: to-plot-type (-> (Listof (Vector String Real))
+                    (Sequenceof (Vector Any (U False Real ivl)))))
+(define (to-plot-type ls)
+  (map (lambda ([v : (Vector String Real)])
+         (vector (ann (vector-ref v 0) Any)
+                 (ann (real-part (vector-ref v 1)) (U False Real ivl)))) ls))

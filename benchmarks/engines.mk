df23ec2456f9cf9e4beb6b89ff7dd55f8c387d8b
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-18 15:29:43 -0400

    Add nodeHarmony as an engine.

--- benchmarks/engines.mk
+++ benchmarks/engines.mk
@@ -1,19 +1,20 @@
 include transform.mk
 
 # Add new JavaScript engines HERE.
+# For spaces, use '@'
 # Add the absolute path to the JS-Engine here:
-ENGINEPATH := $(shell which node)
+ENGINEPATH := $(shell which node)@--harmony_tailcalls "$(shell which node)"
 
-# Add the name of the JS-engine here (shoud not contain ':'):
+# Add the name of the JS-engine here (shoud not contain ':' or '-'):
 # Note that the index must coorespond to the engine path.
-ENGINENAME := node
+ENGINENAME := nodeHarmony node
 
 # Join the data about the engines.
 ENGINEDATA := $(join $(addsuffix :, $(ENGINEPATH)), $(ENGINENAME))
 
 # GETTERS
-GET_EPATH = $(word 1,$(subst :, ,$1))
-GET_ENAME = $(word 2,$(subst :, ,$1))
+GET_EPATH = $(subst @, ,$(word 1,$(subst :, ,$1)))
+GET_ENAME = $(subst @, ,$(word 2,$(subst :, ,$1)))
 
 RUNNABLE := base $(TRANSFORMS)
 

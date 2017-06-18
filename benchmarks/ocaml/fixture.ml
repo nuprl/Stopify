613f53e0394abb00aff6add2d13cedef1d4a18bd
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-18 15:48:08 -0400

    Remove benchmarks (preparing for submodule).

--- benchmarks/ocaml/fixture.ml
+++
@@ -1,5 +0,0 @@
-(* Benchmark looper *)
-let run_n_times (n : int) (f : unit -> unit) : unit =
-  for _ = 1 to n do
-    f ()
-  done
--- benchmarks/transform.mk
+++
@@ -1,7 +0,0 @@
-# Add a new stopify transform here. The name should correspond to the flag
-# used by the stopify binary.
-TRANSFORMS = yield
-
-# Specify the intervals here.
-INTERVALS = 100 150 200
-# 500 750 1000 1250 1500 2000 2500

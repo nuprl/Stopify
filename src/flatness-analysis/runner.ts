1442900011495bdf1be9e49d4bd08b2b4c17bc73
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-22 12:56:56 -0400

    remove flatness runner

--- src/flatness-analysis/runner.ts
+++
@@ -1,7 +0,0 @@
-import { transform } from '../common/helpers'
-import * as flatness from './dependencyGraph'
-import * as fs from 'fs'
-
-const code: string = fs.readFileSync(process.argv[2]).toString();
-
-transform(code, [[flatness]])

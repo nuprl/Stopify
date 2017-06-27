068e59f944943e46b40535fe9b671f62e2c1b454
Sam Baxter <sbaxter@cs.umass.edu>
2017-06-27 10:02:18 -0400

    Add examples of jsc vs. node tail call behavior

---
+++ examples/fapply-tail-sum-node.js
@@ -0,0 +1,14 @@
+'use strict';
+function fapply(f, ...args) {
+  return f(...args);
+}
+
+function sum(x, acc) {
+    if (x === 0) {
+        return acc;
+    } else {
+        return fapply(sum, x-1, acc + x);
+    }
+}
+
+console.log(sum(100000, 0));

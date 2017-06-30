95d8dd352bb3d30347f2ae7e968d0b4a4bdf1ec5
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-30 17:26:44 -0400

    Fix regenerator transform.

--- src/yield/stopifyRegen.ts
+++ src/yield/stopifyRegen.ts
@@ -1,69 +1,17 @@
-const noArrows = require('babel-plugin-transform-es2015-arrow-functions');
-
+import * as stopifyYield from './stopifyYield';
 import { stopifyFunction, stopifyPrint } from '../interfaces/stopifyInterface'
-import * as desugarNew from '../common/desugarNew';
-import * as makeBlockStmt from '../common/makeBlockStmt';
-import * as yieldPass from './yield';
-import * as transformMarked from '../common/transformMarked';
-import { transform } from '../common/helpers';
-import * as markKnown from '../common/markKnownFunctions'
+export const regenStopifyPrint: stopifyPrint = (code: string) => {
 
-// The runtime needs to be stored as a string to allow for client-side
-// compilation.
+  const inter = stopifyYield.yieldStopifyPrint(code)
+  const intermediate = `(${inter})($isStop, $onStop, $onDone, $interval)`
 
-const yieldRuntime = `
-/*
- * The runtime is wrapped in a funtion:
- * function($isStop, $onStop, $onDone, $interval).
- * The output of the yield transform expects $counter to be defined.
- */
-const $yieldCounter = $interval;
-let $counter = 0;
-function $mark_func(f) {
-  f.$isTransformed = true;
-  Object.defineProperty(f.prototype, "constructor", {
-    value: f.prototype.constructor, writable: true
-  })
-  return f;
-};
-
-function $runYield(gen, res = { done: false, value: undefined }) {
-  setTimeout(_ => {
-    if ($isStop()) {
-      return $onStop();
-    }
-    res = gen.next();
-    if (res.done) {
-      return $onDone(res.value);
-    }
-    else {
-      return $runYield(gen, res);
-    }
-  }, 0)
-};
-`
-
-export const regenStopifyPrint: stopifyPrint = (code) => {
-  const plugins = [
-    [noArrows, desugarNew], [makeBlockStmt], [markKnown], [yieldPass],
-    [transformMarked]
-  ];
-  const intermediate = transform(code, plugins);
   const transformed = require('regenerator').compile(intermediate, {
     includeRuntime: true
   }).code;
 
-
-  if(transformed.length < code.length) {
-    throw new Error('Transformed code is smaller than original code')
-  }
-
-  return `
-  function $stopifiedProg($isStop, $onStop, $onDone, $interval) {
-    ${yieldRuntime}
-    ${transformed}
-  }
-  `
+  return `function $stopifiedProg($isStop, $onStop, $onDone, $interval) {
+  ${transformed}
+}`
 }
 
 export const regenStopify: stopifyFunction = (code) => {

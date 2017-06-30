e3b8f36d40e8c2a732731a148ca9b1a98de72ed6
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-30 18:22:51 -0400

    Regenerator first pass eval implementation.

--- src/yield/stopifyRegen.ts
+++ src/yield/stopifyRegen.ts
@@ -1,6 +1,8 @@
 import * as stopifyYield from './stopifyYield';
 import { stopifyFunction, stopifyPrint } from '../interfaces/stopifyInterface'
 
+const includeRuntime = `const $compile_string = require('${__dirname}/stopifyRegen').regenStopifyEval`
+
 export const regenStopifyPrint: stopifyPrint = (code: string) => {
   const transformedData = stopifyYield.yieldStopifyRegen(code)
   const intermediate = transformedData[0]
@@ -10,8 +12,17 @@ export const regenStopifyPrint: stopifyPrint = (code: string) => {
   }).code;
 
   return `function $stopifiedProg($isStop, $onStop, $onDone, $interval) {
+  ${transformedData[1] ? includeRuntime.toString() : ""}
   ${transformed}
 }`
+}
+
+export function regenStopifyEval(code: string): string {
+  const intermediate = stopifyYield.yieldEvalString(code)
+  const transformed: string = require('regenerator').compile(intermediate, {
+  }).code;
+  console.log(transformed)
+  return transformed
 }
 
 export const regenStopify: stopifyFunction = (code) => {

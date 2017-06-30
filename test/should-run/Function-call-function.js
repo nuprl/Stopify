f4c7b5dfee1aac2b050bfa0f9f95602928dfd7d3
Rachit Nigam <rachit.nigam12@gmail.com>
2017-06-30 10:50:22 -0400

    Fix Function-call-function test.

--- test/should-run/Function-call-function.js
+++ test/should-run/Function-call-function.js
@@ -4,6 +4,10 @@ global.g = function (x) {
   return x * 10;
 }
 
-const f = new Function('x', "return g(x)")
+global.h = function (x) {
+  return x + 10;
+}
 
-assert(f(10), 100)
+const f = new Function('x', "const _t = g(x); const _t2 = h(x); return _t + _t2")
+
+assert.equal(f(10), 120)

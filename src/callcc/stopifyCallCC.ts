d024176a1eb9034eaa8ca985933a07af49d0a84d
Arjun Guha <arjun@cs.umass.edu>
2017-07-15 23:00:22 -0400

    Separate pass that only applies callcc
    - callcc pass produces a running program and not a function,
      unlike the stopify passes. The code is mostly from stopifyCallCC.
    - stopifyCallCC has been rewritten to use the callcc pass. It
      still doesn't actually make the program stoppable.
    - The build script places a symlink to the stopify directory in
      node_modules. When we generate code that needs to reference Stopify's
      modules, we should not use local paths. i.e., use
      'stopify/built/src/callcc/runtime' instead of './runtime'. This will
      allow us to run code from other paths and the symlink will let us
      refer to Stopify within the Stopify directory.

--- src/callcc/stopifyCallCC.ts
+++ src/callcc/stopifyCallCC.ts
@@ -1,39 +1,16 @@
+import * as callcc from './callcc';
 import { stopifyFunction, stopifyPrint } from '../interfaces/stopifyInterface';
+import * as babel from 'babel-core';
 
-import * as desugarLoop from '../common/desugarLoop';
-import * as desugarLabel from '../common/desugarLabel';
-import * as desugarSwitch from '../common/desugarSwitch';
-import * as desugarLogical from '../common/desugarLogical';
-import * as makeBlocks from '../common/makeBlockStmt';
-import * as anf from '../common/anf';
 
-import * as label from './label';
-import * as jumper from './jumper';
-import * as declVars from './declVars';
-import * as nameExprs from './nameExprs';
-
-import { transform } from '../common/helpers';
-
-const runtime: string = `
-const $__R = require('${__dirname}/runtime');
-`;
 
 export const callCCStopifyPrint: stopifyPrint = (code, opts) => {
-  const plugins : any[] = [
-    [makeBlocks, nameExprs, desugarLoop, desugarLabel, desugarSwitch,
-      desugarLogical],
-    [anf],
-    [declVars],
-    [label],
-    [jumper],
-  ];
-  const transformed: string = transform(code, plugins, opts).code;
+  const transformOpts = { plugins: [callcc], babelrc: false };
+  const { code: transformed } = babel.transform(code, transformOpts);
 
   return `
 function $stopifiedProg($isStop, $onStop, $onDone, $interval) {
-  ${runtime}
   ${transformed}
-  $__R.runtime($program);
   $onDone();
 }
 `

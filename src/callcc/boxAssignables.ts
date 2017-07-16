947894816eb781193edb5e16747a1776d54e974e
Arjun Guha <arjun@cs.umass.edu>
2017-07-16 13:20:42 -0400

    Tackle assignable arguments

--- src/callcc/boxAssignables.ts
+++ src/callcc/boxAssignables.ts
@@ -2,8 +2,22 @@ import * as t from 'babel-types';
 import * as babel from 'babel-core';
 import { NodePath, Visitor } from 'babel-traverse';
 
-  
 function boxVars(path: NodePath<t.Node>, vars: string[]) {
+  function handleFunction(path: NodePath<t.Function>, body: t.BlockStatement) {
+    const vars0 = vars.filter(x => !path.scope.hasOwnBinding(x));
+    path.skip();
+    const binds = path.scope.bindings;
+    const vars1 = Object.keys(binds).filter(x => !binds[x].constant);
+    path.node.params.forEach(x => {
+      if (x.type === "Identifier" && !binds[x.name].constant) {
+        const init = t.assignmentExpression("=", x, t.arrayExpression([x]));
+        (<any>init).__boxVarsInit__ = true;
+        body.body.unshift(t.expressionStatement(init));
+      }
+    });
+    boxVars(path, [...vars0, ...vars1]);
+  }
+
   const visit: Visitor = {
     VariableDeclarator(path: NodePath<t.VariableDeclarator>) {
       if (path.node.id.type !== "Identifier") {
@@ -13,7 +27,23 @@ function boxVars(path: NodePath<t.Node>, vars: string[]) {
         path.node.init = t.arrayExpression([path.node.init]);
       }
     },
+    AssignmentExpression(path: NodePath<t.AssignmentExpression>) {
+      if ((<any>path.node).__boxVarsInit__) {
+        path.skip();
+      }
+    },
     Identifier(path: NodePath<t.Identifier>) {
+      const parent = path.parent;
+      if (parent.type === "FunctionExpression" ||
+          parent.type === "FunctionDeclaration") {
+        path.skip();
+        return;
+      }
+      if (parent.type === "MemberExpression" &&
+          (<any>parent).property === path.node) {
+        path.skip();
+        return;
+      }
       if (path.parent.type === "VariableDeclarator") {
         path.skip();
         return;
@@ -23,12 +53,11 @@ function boxVars(path: NodePath<t.Node>, vars: string[]) {
         path.skip();
       }
     },
-    Function(path: NodePath<t.Function>) {
-      const vars0 = vars.filter(x => !path.scope.hasOwnBinding(x));
-      path.skip();
-      const binds = path.scope.bindings;
-      const vars1 = Object.keys(binds).filter(x => !binds[x].constant);
-      boxVars(path, [...vars0, ...vars1]);
+    FunctionDeclaration(path: NodePath<t.FunctionDeclaration>) {
+      handleFunction(path, path.node.body);
+    },
+    FunctionExpression(path: NodePath<t.FunctionExpression>) {
+      handleFunction(path, path.node.body);
     }
   }
 

0a7530decd22c4964108222e3ec35a6ecad252b9
Arjun Guha <arjun@cs.umass.edu>
2017-07-15 21:37:04 -0400

    Simpler output from desugarLogical
    This transformation is only needed if the expressions have applications.

--- src/common/desugarLogical.ts
+++ src/common/desugarLogical.ts
@@ -2,17 +2,36 @@ import {NodePath, VisitNode, Visitor} from 'babel-traverse';
 import * as t from 'babel-types';
 import {letExpression} from '../common/helpers';
 
+
+function containsCall<T>(path: NodePath<T>) {
+    let containsCall = false;
+    const containsCallVisitor = {
+        CallExpression(path: NodePath<t.LogicalExpression>) {
+            containsCall = true;
+        }
+    };
+    path.traverse(containsCallVisitor);
+    return containsCall;
+}
+
 export const visitor: Visitor = {
 
     WhileStatement(path: NodePath<t.WhileStatement>) {
+        if (!containsCall(path)) {
+            return;
+        }
         const test = path.node.test;
         path.node.test = t.booleanLiteral(true);
         path.node.body = t.blockStatement(
             [t.ifStatement(test, path.node.body, t.breakStatement())]);
 
     },
-    
+
     LogicalExpression(path: NodePath<t.LogicalExpression>) {
+        if (!containsCall(path)) {
+            return;
+        }
+
         const op = path.node.operator;
         const stmt = path.getStatementParent();
         const r = stmt.scope.generateUidIdentifier(op === "&&" ? "and" : "or");
@@ -36,8 +55,12 @@ export const visitor: Visitor = {
         }
         path.replaceWith(r);
     },
-    
+
     ConditionalExpression(path: NodePath<t.ConditionalExpression>) {
+        if (!containsCall(path)) {
+            return;
+        }
+
         const r = path.scope.generateUidIdentifier("cond");
         const test = path.scope.generateUidIdentifier("test");
 

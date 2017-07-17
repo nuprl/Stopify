5d3c9e9aede22e8013b05f32eb1ca8d2a5e16e74
Rachit Nigam <rachit.nigam12@gmail.com>
2017-07-17 13:00:42 -0400

    Add integration test file.

---
+++ test/should-run-integration-tests.ts
@@ -0,0 +1,29 @@
+import * as f from './testFixtures.js';
+
+// NOTE(rachit): Don't use arrow functions, otherwise timeout doesn't work.
+describe('CPS integration tests', function () {
+  this.timeout(0)
+  f.intTests.forEach(function(filename: string) {
+    it(filename, function () {
+      f.stopifyTest(filename, 'cps')
+    })
+  })
+})
+
+describe('Yield integration tests', function () {
+  this.timeout(0)
+  f.intTests.forEach(function(filename: string) {
+    it(filename, function () {
+      f.stopifyTest(filename, 'yield');
+    })
+  })
+})
+
+describe('Call/CC integration tests', function () {
+  this.timeout(0)
+  f.intTests.forEach(function(filename: string) {
+    it(filename, function () {
+      f.stopifyTest(filename, 'callcc');
+    })
+  })
+})

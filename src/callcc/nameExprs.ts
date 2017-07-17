1b0532c75eb50b543f227d8cdfd4a7a9087239f2
Sam Baxter <sbaxter@cs.umass.edu>
2017-07-17 15:38:15 -0400

    Rename local vars with the same name as function identifiers in callcc

--- src/callcc/nameExprs.ts
+++ src/callcc/nameExprs.ts
@@ -5,6 +5,14 @@ const names: Visitor = {
   FunctionExpression: function (path: NodePath<t.FunctionExpression>): void {
     if (path.node.id === undefined || path.node.id === null) {
       path.node.id = path.scope.generateUidIdentifier('funExpr');
+    } else if (path.scope.hasOwnBinding(path.node.id.name)) {
+      path.scope.rename(path.node.id.name);
+    }
+  },
+
+  FunctionDeclaration: function (path: NodePath<t.FunctionDeclaration>): void {
+    if (path.scope.hasOwnBinding(path.node.id.name)) {
+      path.scope.rename(path.node.id.name);
     }
   },
 };

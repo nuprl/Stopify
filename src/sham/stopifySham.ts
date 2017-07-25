afda6f7726ef9373ae587a326bc7cf656c3de982
Rachit Nigam <rachit.nigam12@gmail.com>
2017-07-25 14:28:52 -0400

    Add missing sham transform.

---
+++ src/sham/stopifySham.ts
@@ -0,0 +1,17 @@
+import { stopifyFunction, stopifyPrint } from '../interfaces/stopifyInterface';
+
+export const shamStopifyPrint: stopifyPrint = (code, opts) => {
+  return `
+  function $stopifiedProg($isStop, $onStop, $onDone, $interval) {
+    ${code}
+  }
+  `
+}
+
+export const shamStopify: stopifyFunction = (code, opts) => {
+  return eval(`
+    (function () {
+      return (${shamStopifyPrint(code, opts)});
+    })()
+  `)
+}

a24a20235116887548712f70b300100b39118eec
Sam Baxter <sbaxter@cs.umass.edu>
2017-08-01 14:12:34 -0400

    Make lazy and retval runtimes extend abstract runtime class

--- src/callcc/retvalRuntime.ts
+++ src/callcc/retvalRuntime.ts
@@ -1,156 +1,111 @@
 import * as common from './runtime';
-export * from './runtime';
 
-export let stack: common.Stack = [];
-export let mode: common.Mode = 'normal';
-
-export function captureCC(f: (k: any) => any): common.Capture {
-  return new common.Capture(f, []);
-}
-
-export function abortCC(f: () => any): common.Discard {
-  return new common.Discard(f);
-}
-
-export function makeCont(stack: common.Stack): (v: any) => common.Restore {
-  return function (v: any) {
-    return new common.Restore([topK(() => v), ...stack]);
+class RetValRuntime extends common.Runtime {
+  constructor() {
+    super();
   }
-}
 
-// Helper function that constructs a top-of-stack frame.
-export function topK(f: () => any): common.KFrameTop {
-  return {
-    kind: 'top',
-    f: () => {
-      stack = [];
-      mode = 'normal';
-      return f();
+  captureCC(f: (k: any) => any): common.Capture {
+    return new common.Capture(f, []);
+  }
+
+  makeCont(stack: common.Stack): (v: any) => common.Restore {
+    return function (v: any) {
+      return new common.Restore([this.topK(() => v), ...this.stack]);
     }
-  };
-}
-
-export function restore(aStack: common.Stack): any {
-  if (aStack.length === 0) {
-    throw new Error(`Can't restore from empty stack`);
   }
-  mode = 'restoring';
-  stack = aStack;
-  return stack[stack.length - 1].f();
-}
 
-export function runtime(body: any): any {
-  if (body instanceof Function) {
-    let res = body();
-    if (res instanceof common.Capture) {
-      // Recursive call to runtime addresses nested continuations. The return
-      // statement ensures that the invocation is in tail position.
-      // At this point, res.stack is the continuation of callCC, but doesn’t have
-      // a top-of-stack frame that actually restores the saved continuation. We
-      // need to apply the function passed to callCC to the stack here, because
-      // this is the only point where the whole stack is ready.
-      return runtime(() =>
+  runtime(body: any): any {
+    if (body instanceof Function) {
+      let res = body();
+      if (res instanceof common.Capture) {
+        // Recursive call to runtime addresses nested continuations. The return
+        // statement ensures that the invocation is in tail position.
+        // At this point, res.stack is the continuation of callCC, but doesn’t have
+        // a top-of-stack frame that actually restores the saved continuation. We
+        // need to apply the function passed to callCC to the stack here, because
+        // this is the only point where the whole stack is ready.
         // Doing res.f makes "this" wrong.
-        restore([topK(() => res.f.call(global, makeCont(res.stack))), ...res.stack]));
-    } else if (res instanceof common.Discard) {
-      return runtime(() => res.f());
-    } else if (res instanceof common.Restore) {
+        return this.runtime(() => res.f.call(global, this.makeCont(res.stack)));
+      } else if (res instanceof common.Discard) {
+        return this.runtime(() => res.f());
+      } else if (res instanceof common.Restore) {
+        // The current continuation has been discarded and we now restore the
+        // continuation in res.
+        return this.runtime(() => {
+          if (res.stack.length === 0) {
+            throw new Error(`Can't restore from empty stack`);
+          }
+          this.mode = 'restoring';
+          this.stack = res.stack;
+          return this.stack[this.stack.length - 1].f();
+        });
+      }
+      return res;
+    } else if (body instanceof common.Discard) {
+      return this.runtime(() => body.f());
+    } else if (body instanceof common.Restore) {
       // The current continuation has been discarded and we now restore the
-      // continuation in res.
-      return runtime(() => {
-        if (res.stack.length === 0) {
+      // continuation in body.
+      return this.runtime(() => {
+        if (body.stack.length === 0) {
           throw new Error(`Can't restore from empty stack`);
         }
-        mode = 'restoring';
-        stack = res.stack;
-        return stack[stack.length - 1].f();
+        this.mode = 'restoring';
+        this.stack = body.stack;
+        return this.stack[this.stack.length - 1].f();
       });
     }
-
-    return res;
-  } else if (body instanceof common.Discard) {
-    return runtime(() => body.f());
-  } else if (body instanceof common.Restore) {
-    // The current continuation has been discarded and we now restore the
-    // continuation in body.
-    return runtime(() => {
-      if (body.stack.length === 0) {
-        throw new Error(`Can't restore from empty stack`);
-      }
-      mode = 'restoring';
-      stack = body.stack;
-      return stack[stack.length - 1].f();
-    });
-  }
-}
-
-export const knownBuiltIns = [Object, Function, Boolean, Symbol, Error, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, URIError, Number, Math, Date, String, RegExp, Array, Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Float64Array, Map, Set, WeakMap, WeakSet];
-
-export function handleNew(constr: any, ...args: any[]) {
-  if (knownBuiltIns.includes(constr)) {
-    return new constr(...args);
   }
 
-  let obj;
-  if (mode === "normal") {
-    obj = Object.create(constr.prototype);
-  } else {
-    const frame = stack[stack.length - 1];
-    if (frame.kind === "rest") {
-      [obj] = frame.locals;
+  handleNew(constr: any, ...args: any[]) {
+    if (common.knownBuiltIns.includes(constr)) {
+      return new constr(...args);
+    }
+
+    let obj;
+    if (this.mode === "normal") {
+      obj = Object.create(constr.prototype);
     } else {
-      throw "bad";
+      const frame = this.stack[this.stack.length - 1];
+      if (frame.kind === "rest") {
+        [obj] = frame.locals;
+      } else {
+        throw "bad";
+      }
+      this.stack.pop();
     }
-    stack.pop();
-  }
 
-  if (mode === "normal") {
-    let _a = constr.apply(obj, args);
-    if (_a instanceof common.Capture) {
-      _a.stack.push({
-        kind: "rest",
-        f: () => handleNew(constr, ...args) ,
-        locals: [obj],
-        index: 0
-      });
-      return _a;
-    } else if (_a instanceof common.Restore) {
-      return _a;
+    if (this.mode === "normal") {
+      let _a = constr.apply(obj, args);
+      if (_a instanceof common.Capture) {
+        _a.stack.push({
+          kind: "rest",
+          f: () => this.handleNew(constr, ...args) ,
+          locals: [obj],
+          index: 0
+        });
+        return _a;
+      } else if (_a instanceof common.Restore) {
+        return _a;
+      }
     }
-  }
-  else {
-    let _a = stack[stack.length - 1].f.apply(obj, []);
-    if (_a instanceof common.Capture) {
-      _a.stack.push({
-        kind: "rest",
-        f: () => handleNew(constr, ...args) ,
-        locals: [obj],
-        index: 0
-      });
-      return _a;
-    } else if (_a instanceof common.Restore) {
-      return _a;
+    else {
+      let _a = this.stack[this.stack.length - 1].f.apply(obj, []);
+      if (_a instanceof common.Capture) {
+        _a.stack.push({
+          kind: "rest",
+          f: () => this.handleNew(constr, ...args) ,
+          locals: [obj],
+          index: 0
+        });
+        return _a;
+      } else if (_a instanceof common.Restore) {
+        return _a;
+      }
     }
-  }
-  return obj;
-}
-
-let countDown: number | undefined;
-
-export function resume(result: any) {
-  return setTimeout(() => runtime(result), 0);
-}
-
-export function suspend(interval: number, top: any): any {
-  if (Number.isNaN(interval)) {
-    return;
-  }
-  if (countDown === undefined) {
-    countDown = interval;
-  }
-  if (--countDown === 0) {
-    countDown = interval;
-    return captureCC(top);
+    return obj;
   }
 }
 
+export default new RetValRuntime();

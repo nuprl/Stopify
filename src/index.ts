c0fce035449651d2a85c4db62d781e8e956054ae
Sam Baxter <sbaxter@cs.umass.edu>
2017-08-02 11:17:25 -0400

    Refactor main package exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import R from './callcc/eagerRuntime';
+export {default as eager} from './callcc/eagerRuntime';
+export {default as lazy} from './callcc/lazyRuntime';
+export {default as retval} from './callcc/retvalRuntime';
+export * from './callcc/runtime';
 
-module.exports.default = R;
+import CallCC from './callcc/callcc';
+
+export default CallCC;

41c041187aeea561b2c8aca398e6f3f72a3f98d8
Arjun Guha <arjun@cs.umass.edu>
2017-08-05 18:18:47 -0400

    Test that browser supports tail calls

--- test/browser-integration.test.ts
+++ test/browser-integration.test.ts
@@ -1,7 +1,12 @@
 import * as f from './testFixtures.js';
+import * as glob from 'glob';
 
 describe('In-browser tests', function () {
   f.intTests.forEach(function(filename: string) {
     f.browserTest(filename, "lazy");
+  });
+  
+  glob.sync('test/browser/*.js').forEach(file => {
+    f.browserTest(file, 'lazy');
   });
 });

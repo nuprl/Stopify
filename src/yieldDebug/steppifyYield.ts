b170df7344700d1e6cb7e105c1ed671611d3af75
Arjun Guha <arjun@cs.umass.edu>
2017-08-22 17:09:57 -0400

    Revert "Remove opts from markFlat. Update markFlatApp."
    This reverts commit d36b8d03de5206b42a9e23f1e929d68cc9929731.

--- src/yieldDebug/steppifyYield.ts
+++ src/yieldDebug/steppifyYield.ts
@@ -6,6 +6,7 @@ import * as makeBlockStmt from '../common/makeBlockStmt';
 import * as yieldDebug from './yieldDebug';
 import * as transformMarked from '../common/transformMarked';
 import { transformWithLines } from '../common/helpers';
+import * as markKnown from '../common/markKnownFunctions'
 
 class YieldSteppify implements Steppable {
   private original: string;
@@ -29,7 +30,7 @@ class YieldSteppify implements Steppable {
     this.original = code;
 
     const plugins = [
-      [noArrows], [makeBlockStmt], [yieldDebug],
+      [noArrows], [makeBlockStmt], [markKnown], [yieldDebug],
       [transformMarked]
     ];
     this.transformed = transformWithLines(code, plugins, [])

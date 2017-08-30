9f0742db907f8fcb0d057e8734bb25a2eb4a6e4e
Sam Baxter <sbaxter@cs.umass.edu>
2017-08-30 12:14:31 -0400

    Use headless chrome/firefox browsers

--- src/runInBrowser.ts
+++ src/runInBrowser.ts
@@ -1,9 +1,12 @@
 // NOTE(arjun): If this script doesn't work, you probably don't have Xvfb.
 import * as selenium from 'selenium-webdriver';
+import * as chrome from 'selenium-webdriver/chrome';
 import * as path from 'path';
 import * as runtime from './runtime/default';
 import * as os from 'os';
 const xvfb = require('xvfb'); // No type definitions as of 8/2/2017
+
+process.env.MOZ_HEADLESS = "1";
 
 const stdout = process.stdout;
 const args = process.argv.slice(2);
@@ -19,16 +22,17 @@ const src = 'file://' + path.resolve('.', opts.filename) +
 
 let vfb: any;
 
-if (os.platform() === 'linux') {
-  vfb = new xvfb({ reuse: true });
-  vfb.startSync();
-}
+// NOTE(sam): No typing for `headless()` option as of 8/30/2017.
+// I've opened a PR to DefinitelyTyped to fix this.
+// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/19463
+const chromeOpts = (<any>new chrome.Options()).headless();
 
 const loggingPrefs = new selenium.logging.Preferences();
 loggingPrefs.setLevel('browser', 'all');
 const driver = new selenium.Builder()
   .forBrowser(opts.env)
   .setLoggingPrefs(loggingPrefs)
+  .setChromeOptions(chromeOpts)
   .build();
 
 driver.get(src);

ea5b6505fc208833a77cb2917e28e2559b1371a1
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-06 16:27:55 -0400

    Remove yield code and tests.

--- test/should-run.test.ts
+++ test/should-run.test.ts
@@ -1,19 +1,9 @@
 import * as f from './testFixtures.js';
-import * as path from 'path';
-
-describe('Yield transformation tests', function () {
-
-  f.unitTests.forEach(function(filename: string) {
-    it(`${filename} (yield)`, function () {
-      f.stopifyTest(filename, 'yield', 500);
-    })
-  })
-})
 
 describe('call/cc', function() {
   f.unitTests.forEach(function(filename: string) {
     f.callCCTest(filename, "lazy");
     f.callCCTest(filename, "eager");
     f.callCCTest(filename, "retval");
-  });  
+  });
 });
--- test/should-stop.test.ts
+++ test/should-stop.test.ts
@@ -1,13 +1,5 @@
 import * as f from './testFixtures.js';
 
-describe('Yield stopping tests', function () {
-  f.stopTests.forEach(function(filename: string) {
-    it(filename, function () {
-      f.stopProgramTest(filename, 'yield');
-    })
-  })
-})
-
 describe('Call/CC stopping tests', function () {
   f.stopTests.forEach((filename: string) => {
     f.stopCallCCTest(filename, 'lazy');

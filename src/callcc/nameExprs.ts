28e886dec5b500d45322aa89300a30699b824779
Sam Baxter <sbaxter@cs.umass.edu>
2017-09-07 15:19:39 -0400

    Turn ObjectMethods into properties set to function expressions

--- src/callcc/nameExprs.ts
+++ src/callcc/nameExprs.ts
@@ -3,6 +3,13 @@ import * as t from 'babel-types';
 import * as fastFreshId from '../fastFreshId';
 
 const names: Visitor = {
+  ObjectMethod: function (path: NodePath<t.ObjectMethod>): void {
+    path.replaceWith(t.objectProperty(path.node.key,
+      t.functionExpression(fastFreshId.fresh('funExpr'),
+        path.node.params, path.node.body),
+      path.node.computed));
+  },
+
   FunctionExpression: function (path: NodePath<t.FunctionExpression>): void {
     if (path.node.id === undefined || path.node.id === null) {
       path.node.id = fastFreshId.fresh('funExpr');

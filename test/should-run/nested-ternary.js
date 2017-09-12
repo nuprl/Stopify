dcd944c5002bb294c87d70f48f564c882821fea1
Sam Baxter <sbaxter@cs.umass.edu>
2017-09-12 18:43:33 -0400

    Make eval-based test fixture happy

--- test/should-run/nested-ternary.js
+++ test/should-run/nested-ternary.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 
 function Foo() {}
 function Bar() {}
+function Baz() {}
 
 function foo() {
-  return this.left instanceof Foo ? new Baz(this.left.error) :
-    this.right instanceof Bar ?  new Baz(this.right.error) : 7;
+  const o = {};
+  return (o.left instanceof Foo) ? new Baz(o.left.error) :
+    ((o.right instanceof Bar) ?  new Baz(o.right.error) : 7);
 }
 
 assert.equal(foo(), 7);

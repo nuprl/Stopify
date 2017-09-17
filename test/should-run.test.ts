24162784bcecf6f88e98ccf060b6f87ec452d084
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-17 13:48:57 -0400

    Disable retval and eager tests. DO NOT MERGE.

--- test/should-run.test.ts
+++ test/should-run.test.ts
@@ -3,7 +3,7 @@ import * as f from './testFixtures.js';
 describe('call/cc', function() {
   f.unitTests.forEach(function(filename: string) {
     f.callCCTest(filename, "lazy");
-    f.callCCTest(filename, "eager");
-    f.callCCTest(filename, "retval");
+    //f.callCCTest(filename, "eager");
+    //f.callCCTest(filename, "retval");
   });
 });

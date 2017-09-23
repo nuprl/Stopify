64359d6fdf443e6f0324f66b03b2937a72f409e8
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-23 06:22:20 -0400

    Test lazy + new=direct. (#158)

--- test/should-run.test.ts
+++ test/should-run.test.ts
@@ -3,6 +3,7 @@ import * as f from './testFixtures.js';
 describe('call/cc', function() {
   f.unitTests.forEach(function(filename: string) {
     f.callCCTest(filename, "lazy");
+    f.callCCTest(filename, "lazy", "--new direct")
     f.callCCTest(filename, "eager");
     f.callCCTest(filename, "retval");
   });

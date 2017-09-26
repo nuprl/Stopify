9efd9d38a43e41dc289d82fa79005b803b4e5ed2
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-26 08:49:14 -0400

    rewrite, revert naming fun-expr

--- src/callcc/markBlocks.ts
+++ src/callcc/markBlocks.ts
@@ -1,64 +1,90 @@
 import * as t from "babel-types";
 import { NodePath } from "babel-traverse";
 
+// undefined stands for mergeable blocks.
+type MergeMarks = 'tail' | undefined
+type MergeMarkable<T> = T & { mergemark: MergeMarks }
+
 // We would've needed to check that each of the argument is also a value,
 // but ANF guarantees that all of the args are named.
 function isTailCall(arg: t.Expression) {
   return (t.isCallExpression(arg) || t.isNewExpression(arg))
 }
 
-// NOTE(rachit): This assumes that the functionExpression has been named and
-// does not occur in a call expression. Weird behaviour when function expression
-// is not named.
-function functionMarkingVisitor(path: NodePath<t.FunctionExpression|t.FunctionDeclaration>) {
-  (<any>path.node).nomerge = true;
-  let parent: NodePath<t.Node> = path.findParent(t.isStatement);
-  while (parent && !t.isFunction(parent)) {
-    (<any>parent.node).nomerge = true
-    parent = parent.findParent(t.isStatement)
+function genBlocks(path: NodePath<t.FunctionDeclaration|t.FunctionExpression>) {
+  let body: t.Statement[] = path.node.body.body
+
+  // TODO(rachit): We are doing this calculation twice now. Let's make it a property
+  const afterDecls = Math.max(body.findIndex(e =>
+    !(<any>e).__boxVarsInit__ && !(<any>e).lifted), 0);
+
+  const groups: t.Statement[][] = [[]];
+
+  for (let i = afterDecls; i < body.length; i++) {
+    if ((<MergeMarkable<t.Statement>>body[i]).mergemark === 'tail') {
+      groups.push([body[i]])
+      groups.push([])
+    }
+    else {
+      groups[groups.length - 1].push(body[i])
+    }
   }
+
+  // Stuff after hoisted vars
+  const post = groups.filter(x => x.length !== 0).map(g => {
+    if (g.length === 1 && (<MergeMarkable<t.Statement>>g[0]).mergemark == 'tail') {
+      return g[0]
+    }
+    else {
+      const block = t.blockStatement(g);
+      (<any>block).merge = true;
+      return block
+    }
+  });
+  path.node.body.body = [...body.slice(0, afterDecls), ...post]
+  path.skip()
 }
 
-const markingVisitor = {
-  FunctionExpression: functionMarkingVisitor,
-  FunctionDeclaration: functionMarkingVisitor,
-  ReturnStatement(path: NodePath<t.ReturnStatement>) {
-    const arg = path.node.argument;
+const bodyVisitor = {
+  ReturnStatement(path: NodePath<MergeMarkable<t.ReturnStatement>>) {
     // If the argument of return is a tail call, then mark every statement 
     // parent containing this upto the function as 'un-flattenable' (i.e.
     // try blocks around it can't be merged.)
-    if (isTailCall(arg)) {
-      let parent: NodePath<t.Node> = path
-      while(parent && !t.isFunction(parent)) {
-        (<any>parent.node).nomerge = true
-        parent = parent.findParent(t.isStatement)
+    if (isTailCall(path.node.argument)) {
+      let parent: NodePath<MergeMarkable<t.Node>> = path
+      while (parent && !t.isFunction(parent)) {
+        parent.node.mergemark = 'tail';
+        parent = <NodePath<MergeMarkable<t.Node>>>parent.findParent(t.isStatement)
       }
     }
-  }
+  },
 }
 
 const visitor = {
+  Program(path: NodePath<t.Program>) {
+    path.traverse(bodyVisitor)
+  },
+  FunctionDeclaration: {
+    exit: genBlocks
+  },
+  FunctionExpression: {
+    exit: genBlocks
+  },
   BlockStatement: {
     /**
      * This generates blocks around groups of mergeable children. These 
      * blocks can simply be plopped in as the block for the try in jumper for
      * the lazy transform.
      */
-    enter(path: NodePath<t.BlockStatement>) {
-      if((<any>path.node).merge && !t.isFunction(path.parent)) {
-        path.skip()
+    exit(path: NodePath<MergeMarkable<t.BlockStatement>>) {
+      if (path.node.mergemark !== 'tail' && (<any>path.getStatementParent().node).mergemark !== 'tail' || t.isFunction(path.parent)) {
         return
       }
-      path.traverse(markingVisitor)
-
       const { body } = path.node
-      const afterDecls = Math.max(body.findIndex(e =>
-        !(<any>e).__boxVarsInit__ && !(<any>e).lifted), 0);
-
       const groups: t.Statement[][] = [[]];
 
-      for (let i = afterDecls; i < body.length; i++) {
-        if((<any>body[i]).nomerge) {
+      for (let i = 0; i < body.length; i++) {
+        if ((<MergeMarkable<t.Statement>>body[i]).mergemark === 'tail') {
           groups.push([body[i]])
           groups.push([])
         }
@@ -67,9 +93,8 @@ const visitor = {
         }
       }
 
-      // Stuff after hoisted vars
       const post = groups.filter(x => x.length !== 0).map(g => {
-        if (g.length === 1 && (<any>g[0]).nomerge) {
+        if (g.length === 1 && (<any>g[0]).mergemark === 'tail') {
           return g[0]
         }
         else {
@@ -78,7 +103,8 @@ const visitor = {
           return block
         }
       });
-      path.node.body = [...body.slice(0, afterDecls), ...post]
+
+      path.node.body = post
     }
   }
 }

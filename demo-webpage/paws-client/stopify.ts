3cd6c58099f1458fd6ed7af7e29a309ab4e029bf
Sam Baxter <sbaxter@cs.umass.edu>
2017-09-27 15:07:33 -0400

    Hook up stepping interface in demo

---
+++ demo-webpage/paws-client/stopify.ts
@@ -0,0 +1,99 @@
+'use strict';
+
+import * as ace from 'brace';
+import { CompilerClient } from '../compilers/compiler';
+import { BuckleScript } from '../compilers/bucklescript-client';
+import { Cljs } from '../compilers/clojurescript-client';
+import { ScalaJS } from '../compilers/scalajs-client';
+import { JavaScript } from '../compilers/javascript-client';
+require('brace/mode/ocaml');
+require('brace/mode/clojure');
+require('brace/mode/scala')
+require('brace/mode/javascript')
+require('brace/theme/monokai');
+const Range = ace.acequire('ace/range').Range;
+
+// TODO(rachit): Hack to share the editor with the runner. Should probably
+// be fixed.
+const editor = ace.edit('editor');
+editor.setTheme('ace/theme/monokai');
+editor.setFontSize('15')
+
+let lastLineMarker: number | null = null;
+function editorSetLine(n: number) {
+    if (lastLineMarker !== null) {
+      editor.session.removeMarker(lastLineMarker);
+    }
+    lastLineMarker = editor.session.addMarker(
+        new Range(n, 0, n, 1),
+        "myMarker", "fullLine", false);
+}
+
+window.addEventListener('message', evt => {
+    editorSetLine(evt.data);
+});
+
+interface supportedLangs {
+  [lang: string]: CompilerClient
+}
+
+const defaultLang = 'OCaml';
+
+const langs : supportedLangs = {
+  OCaml: BuckleScript,
+  ClojureScript: Cljs,
+  ScalaJS: ScalaJS,
+  JavaScript: JavaScript,
+};
+
+editor.getSession().setMode(langs[defaultLang].aceMode);
+editor.setValue(langs[defaultLang].defaultCode);
+
+let iframe = <HTMLIFrameElement>document.getElementById('iframeContainer')!;
+function loadJavaScript(js: string) {
+  iframe.contentWindow.postMessage({ code: js }, '*');
+}
+
+function compileRequest() {
+  const languageSelect = <any>document.getElementById("language-selection");
+  const val = languageSelect.value;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', langs[val].compileUrl);
+  xhr.send(editor.getValue());
+  xhr.addEventListener('load', () => {
+    loadJavaScript(xhr.responseText);
+  });
+}
+
+function setupCompile() {
+  document.getElementById("compile")!.addEventListener('click', () => {
+    compileRequest();
+  });
+}
+
+function selectLanguage() {
+  const languageSelect = <any>document.getElementById("language-selection");
+  languageSelect.addEventListener('input', () => {
+    const val = (<any>document.getElementById("language-selection")).value;
+    console.log(langs[val])
+    editor.getSession().setMode(langs[val].aceMode);
+    editor.setValue(langs[val].defaultCode);
+  });
+}
+
+setupCompile();
+selectLanguage();
+
+function setupButton(buttonId: string, eventName: string) {
+  (<Node>document.getElementById(buttonId)).addEventListener('click', () => {
+    if (iframe === null) {
+      return;
+    }
+
+    iframe.contentWindow.postMessage(eventName, '*');
+  });
+}
+
+setupButton('stop', 'stop')
+setupButton('run', 'run')
+setupButton('step', 'step')

7c7979002190426058b23e15687691d923aee1db
Sam Baxter <sbaxter@cs.umass.edu>
2017-09-29 09:53:17 -0400

    Auto-scroll textarea to bottom

--- src/runtime/default.ts
+++ src/runtime/default.ts
@@ -98,10 +98,14 @@ class Default {
       const data = <HTMLTextAreaElement>document.getElementById('data')!;
       console.log = function (str: any) {
         data.value = data.value + str + '\n';
+        const evt = new Event('change');
+        data.dispatchEvent(evt);
       }
       window.onerror = (message: any) => {
         data.value = data.value + '\nAn error occurred:\n' + message + '\n';
         window.document.title = "done"
+        const evt = new Event('change');
+        data.dispatchEvent(evt);
       }
     }
 

c8b6409665c4a2008a0d445d14dda151c19581ef
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-30 22:06:52 -0400

    fix copySync in stopify-ide

--- stopify-ide/install.js
+++ stopify-ide/install.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const { copyFileSync } = require('fs');
+const { copySync } = require('fs-extra');
 
 const browserify = './node_modules/.bin/browserify';
 execSync('./node_modules/.bin/tsc');
@@ -8,4 +8,4 @@ execSync(`${browserify} dist/paws-client/stopify.js -o dist/stopify.js`);
 execSync(`${browserify} dist/paws-client/container.js -o dist/container.js`);
 execSync(`${browserify} dist/paws-client/runner.js -o dist/runner.js`);
 
-copyFileSync('../stopify/dist/stopify.bundle.js', 'dist/stopify.bundle.js');
+copySync('../stopify/dist/stopify.bundle.js', 'dist/stopify.bundle.js');

44c31eeb65909fc9c01f0f158a3eac7f86e25af3
Rachit Nigam <rachit.nigam12@gmail.com>
2017-10-15 14:46:08 -0400

    fix handleNew

--- stopify/src/callcc/lazyRuntime.ts
+++ stopify/src/callcc/lazyRuntime.ts
@@ -50,63 +50,30 @@ export class LazyRuntime extends common.Runtime {
       }
       else if (exn instanceof common.Capture) {
         return this.runtime(() =>
-          exn.f.call(global, this.makeCont([...exn.stack, ...this.stack]), exn.stack))
+          exn.f.call(
+            global, this.makeCont([...exn.stack, ...this.stack]), exn.stack))
       }
       throw exn // user exception
     }
   }
-
-  // runtime(body: () => any): any {
-  //   try {
-  //     body();
-  //     assert(this.mode, 'executing completed in restore mode');
-  //   }
-  //   catch (exn) {
-  //     if (exn instanceof common.Capture) {
-  //       this.capturing = false;
-  //       // Recursive call to runtime addresses nested continuations. The return
-  //       // statement ensures that the invocation is in tail position.
-  //       // At this point, exn.stack is the continuation of callCC, but doesn’t have
-  //       // a top-of-stack frame that actually restores the saved continuation. We
-  //       // need to apply the function passed to callCC to the stack here, because
-  //       // this is the only point where the whole stack is ready.
-  //       // Doing exn.f makes "this" wrong.
-  //       return this.runtime(() => exn.f.call(global, this.makeCont(exn.stack)));
-  //     } else if (exn instanceof common.Discard) {
-  //       return this.runtime(() => exn.f());
-  //     } else if (exn instanceof common.Restore) {
-  //       // The current continuation has been discarded and we now restore the
-  //       // continuation in exn.
-  //       return this.runtime(() => {
-  //         if (exn.stack.length === 0) {
-  //           throw new Error(`Can't restore from empty stack`);
-  //         }
-  //         this.mode = false;
-  //         this.stack = exn.stack;
-  //         this.stack[this.stack.length - 1].f();
-  //       });
-  //     } else {
-  //       throw exn; // userland exception
-  //     }
-  //   }
-  // }
 
   handleNew(constr: any, ...args: any[]) {
     if (common.knownBuiltIns.includes(constr)) {
       return new constr(...args);
     }
 
-    let obj;
+    let obj, $value;
     if (this.mode) {
       obj = Object.create(constr.prototype);
     } else {
-      const frame = this.stack[this.stack.length - 1];
+      const frame = this.stack[0];
       if (frame.kind === "rest") {
         [obj] = frame.locals;
+        $value = frame.value;
       } else {
         throw "bad";
       }
-      this.stack.pop();
+      this.stack.shift();
     }
 
     let result: any;
@@ -115,7 +82,7 @@ export class LazyRuntime extends common.Runtime {
         result = constr.apply(obj, args);
       }
       else {
-        result = this.stack[this.stack.length - 1].f.apply(obj, []);
+        result = $value
       }
     }
     catch (exn) {

491885ba29f9a5b98e74dfcc94b9524689e41727
Rachit Nigam <rachit.nigam12@gmail.com>
2017-10-16 19:31:49 -0400

    global target

--- stopify/src/callcc/jumper.ts
+++ stopify/src/callcc/jumper.ts
@@ -135,7 +135,6 @@ function func(path: NodePath<Labeled<FunctionT>>): void {
   const newBody = t.blockStatement([
     ...pre,
     letExpression($value, t.nullLiteral()),
-    letExpression(target, t.nullLiteral()),
     ifRestoring,
     captureClosure,
     ...mayMatArgs,

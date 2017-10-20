7d67379792622a50b46dd899f4d0697f0437d9fd
Arjun Guha <arjun@cs.umass.edu>
2017-10-19 21:46:48 -0400

    suspend nesting hack

--- stopify/src/callcc/suspendStop.ts
+++ stopify/src/callcc/suspendStop.ts
@@ -5,6 +5,31 @@ import {letExpression} from '../common/helpers';
 const opts = t.identifier("$opts");
 const result = t.identifier("$result");
 
+const directSuspendVisitor: Visitor = {
+  Function(path: NodePath<t.Function>) {
+    path.skip();
+  },
+  Loop(path: NodePath<t.Loop>) {
+    this.directSuspend = true;
+    path.skip();
+  },
+  IfStatement(path: NodePath<t.IfStatement>) {
+    path.skip();
+  },
+  SwitchStatement(path: NodePath<t.SwitchStatement>) {
+    path.skip();
+  }
+};
+
+/**
+ * Traverses children of `path` and returns true if it contains any applications.
+ */
+export function directSuspend<T>(path: NodePath<T>) {
+  let o = { directSuspend: false };
+  path.traverse(directSuspendVisitor, o);
+  return o.directSuspend;
+}
+
 function handleBlock(body: t.BlockStatement) {
   body.body.unshift(t.expressionStatement(
     t.callExpression( t.memberExpression(t.identifier("$__R"),
@@ -17,6 +42,9 @@ type BlockBody = {
 
 function handleFunction(path: NodePath<t.Node> & BlockBody) {
   if ((<any>path.node).mark === 'Flat') {
+    return;
+  }
+  if (directSuspend(path.get('body'))) {
     return;
   }
   handleBlock(path.node.body);
@@ -33,6 +61,9 @@ const insertSuspend: Visitor = {
 
   Loop(path: NodePath<t.Loop>) {
     if (path.node.body.type === "BlockStatement") {
+      if (directSuspend(path.get('body'))) {
+        return;
+      }
       handleBlock(path.node.body);
     }
     else {

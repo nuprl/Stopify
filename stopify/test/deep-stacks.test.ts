cfdb3e0e460746c9bd2cb1f522e765cd84855870
Rachit Nigam <rachit.nigam12@gmail.com>
2017-10-28 14:39:55 -0400

    f

--- stopify/test/deep-stacks.test.ts
+++ stopify/test/deep-stacks.test.ts
@@ -1,7 +1,7 @@
 import * as f from './testFixtures.js';
 
 describe('deep-stacks', function() {
-  f.deeptests.forEach(function(filename: string) {
+  f.deepTests.forEach(function(filename: string) {
     f.callCCTest(filename, "lazyDeep", "", "-d 1000");
   });
 });

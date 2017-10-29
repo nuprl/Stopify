3b23de01bd607b2adbb52895124c9b3a688e0446
Arjun Guha <arjun@cs.umass.edu>
2017-10-29 19:49:52 -0400

    IDE: I/O in build script

--- stopify-ide/install.js
+++ stopify-ide/install.js
@@ -1,11 +1,14 @@
 const { execSync } = require('child_process');
 const { copySync } = require('fs-extra');
 
+function exec(line) {
+  execSync(line, { stdio: 'inherit' });
+}
 const browserify = './node_modules/.bin/browserify';
-execSync('./node_modules/.bin/tsc');
+exec('./node_modules/.bin/tsc');
 
-execSync(`${browserify} dist/paws-client/stopify.js -o dist/stopify.js`);
-execSync(`${browserify} dist/paws-client/container.js -o dist/container.js`);
-execSync(`${browserify} dist/paws-client/runner.js -o dist/runner.js`);
+exec(`${browserify} dist/paws-client/stopify.js -o dist/stopify.js`);
+exec(`${browserify} dist/paws-client/container.js -o dist/container.js`);
+exec(`${browserify} dist/paws-client/runner.js -o dist/runner.js`);
 
 copySync('../stopify/dist/stopify.bundle.js', 'dist/stopify.bundle.js');

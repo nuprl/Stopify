52266f1f770ed380e53d68370365ae6c67125f4e
Arjun Guha <arjun@cs.umass.edu>
2017-11-23 07:19:03 -0500

    WIP

--- stopify-compiler-cloud-function/bin/init-bucket.sh
+++ stopify-compiler-cloud-function/bin/init-bucket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-BUCKET=`jq -r .output-bucket $DIR/../config.json`
+BUCKET=`jq -r '.["output-bucket"]' $DIR/../config.json`
 gsutil mb -c Regional -l us-central1 gs://$BUCKET
 gsutil defacl ch -u AllUsers:R gs://$BUCKET

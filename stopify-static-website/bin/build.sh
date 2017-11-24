de430a44f485cf934998c1aeeb7d4d555b8e9c1d
Arjun Guha <arjun@cs.umass.edu>
2017-11-24 14:38:19 -0500

    x

--- stopify-static-website/bin/build.sh
+++ stopify-static-website/bin/build.sh
@@ -3,9 +3,9 @@ set -x
 set -e
 BROWSERIFY=./node_modules/.bin/browserify
 
+jekyll build # Must run first, or generated .js files get clobbered
 ./node_modules/.bin/tsc
 
 $BROWSERIFY -d dist/stopify.js -o dist/stopify.js
 $BROWSERIFY -d dist/container.js -o dist/container.js
 cp ../stopify/dist/stopify.bundle.js dist/stopify.bundle.js
-cp www/* dist/

93c863db2cc71bedc9c919010c1e9a8b469a8a41
Rachit Nigam <rachit.nigam12@gmail.com>
2017-12-04 00:13:11 -0500

    final pre-IBM push

--- languages.js
+++ languages.js
@@ -58,9 +58,16 @@ object Runner extends JSApp {
     },
     Python: {
         aceMode: 'python',
-        defaultCode: `def main():
-    print("hello")
-main()`,
+        defaultCode: `def run_forever():
+    i = 0
+    while(True):
+        if i > 10000:
+            i = 0
+        i += 1
+        print i
+
+run_forever()
+    `,
         compileUrl: 'https://us-central1-arjun-umass.cloudfunctions.net/stopify/pyjs'
     }
 };

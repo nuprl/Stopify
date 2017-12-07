6150cd6b688320687afff0b3c3a59f5158f43a0e
Arjun Guha <arjun@cs.umass.edu>
2017-12-07 08:11:31 -0500

    Some comments

--- stopify/src/callcc/nameExprs.ts
+++ stopify/src/callcc/nameExprs.ts
@@ -13,13 +13,23 @@ const names: Visitor = {
   FunctionExpression: function (path: NodePath<t.FunctionExpression>): void {
     if (path.node.id === undefined || path.node.id === null) {
       path.node.id = fastFreshId.fresh('funExpr');
-    } else if (path.scope.hasOwnBinding(path.node.id.name) &&
+    }
+    /*
+     * This deals with the following kind of code:
+     *
+     *   function F() { var F; }
+     *
+     * We need to able to reference F within its body to restore its stack
+     * frame. Therefore, we rename the local variable F.
+     */
+    else if (path.scope.hasOwnBinding(path.node.id.name) &&
       <any>path.scope.bindings[path.node.id.name].kind != 'local') {
-      const new_id = fastFreshId.fresh('funExpr');
+      const new_id = fastFreshId.fresh('x');
       path.scope.rename(path.node.id.name, new_id.name);
     }
   },
-
+  // NOTE(arjun): Dead code? I think no FunctionDeclarations exist at this
+  // point.
   FunctionDeclaration: function (path: NodePath<t.FunctionDeclaration>): void {
     if (path.scope.hasOwnBinding(path.node.id.name) &&
       <any>path.scope.bindings[path.node.id.name].kind != 'local') {

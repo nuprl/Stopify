fbcd279d34271e3a920303a5f402a64678613284
Arjun Guha <arjun@cs.umass.edu>
2017-12-08 03:02:12 -0500

    Updated website

--- languages.js
+++ languages.js
@@ -63,9 +63,16 @@ object Runner extends JSApp {
     Python: {
         stepSupported: false,
         aceMode: 'python',
-        defaultCode: `def main():
-    print("hello")
-main()`,
+        defaultCode: `def run_forever():
+    i = 0
+    while(True):
+        if i > 10000:
+            i = 0
+        i += 1
+        print i
+
+run_forever()
+    `,
         compileUrl: 'https://us-central1-arjun-umass.cloudfunctions.net/stopify/pyjs'
     }
 };

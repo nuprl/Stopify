994f0d394ef291730926ec37c883a7510aa3d661
Arjun Guha <arjun@cs.umass.edu>
2017-12-10 08:46:13 -0500

    Disable stepping in Dart

--- stopify-static-website/ts/languages.ts
+++ stopify-static-website/ts/languages.ts
@@ -10,7 +10,7 @@ const compilerBase = 'https://us-central1-arjun-umass.cloudfunctions.net/stopify
 
 export const langs: { [name: string]: Language } = {
   'Dart': {
-    stepSupported: true,
+    stepSupported: false,
     compileUrl: `${compilerBase}/dart2js`,
     aceMode: 'dart',
     defaultCode: `

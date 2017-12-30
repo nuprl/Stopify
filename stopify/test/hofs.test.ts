d429e6fca9d4b27e4fdd236fc3f9cee179d1620e
Arjun Guha <arjun@cs.umass.edu>
2017-12-30 07:25:15 +0530

    x

--- stopify/test/hofs.test.ts
+++ stopify/test/hofs.test.ts
@@ -11,7 +11,7 @@ describe('separate compilation using --hofs=builtin', () => {
     test(src, () => {
       const { name: dst } = tmp.fileSync({ dir: ".", postfix: '.js' });
       execSync(`./bin/compile -t lazy --hofs=builtin ${src} ${dst}`);
-      const r = execSync(`./bin/browser --env chrome -t lazy --estimator countdown -y 1 ${dst}`);
+      const r = execSync(`./bin/browser chrome -t lazy --estimator countdown -y 1 ${dst}`);
       fs.unlinkSync(dst);
     });
   }

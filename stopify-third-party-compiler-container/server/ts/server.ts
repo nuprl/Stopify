b38f419ea0df230e53a517dc59d4bd7c07599218
Arjun Guha <arjun@cs.umass.edu>
2018-02-06 12:44:56 -0800

    Unified all compilers into a single container

--- stopify-third-party-compiler-container/server/ts/server.ts
+++ stopify-third-party-compiler-container/server/ts/server.ts
@@ -5,6 +5,8 @@ import * as pyjs from './pyjs';
 import * as emscripten from './emscripten';
 import * as bucklescript from './bucklescript';
 import * as clojurescript from './clojurescript';
+import * as scalajs from './scalajs';
+import * as dart2js from './dart2js';
 
 export const app = express();
 app.use(morgan('short'));
@@ -27,3 +29,7 @@ compiler('/pyjs', pyjs.compile);
 compiler('/emscripten', emscripten.compile);
 compiler('/bucklescript', bucklescript.compile);
 compiler('/clojurescript', clojurescript.compile);
+compiler('/scalajs', scalajs.compile);
+compiler('/dart2js', dart2js.compile);
+
+app.listen(8080);

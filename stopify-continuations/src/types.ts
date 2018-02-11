c9ecd043c1c4aec6614fe2c0a9f36813228f43d6
Arjun Guha <arjun@cs.umass.edu>
2018-02-11 13:19:04 -0500

    Made compileFunction optional

--- stopify-continuations/src/types.ts
+++ stopify-continuations/src/types.ts
@@ -2,7 +2,7 @@ export type CaptureMethod = 'eager' | 'retval' | 'lazy' | 'original' | 'fudge';
 export type HandleNew = 'direct' | 'wrapper'
 
 export interface CompilerOpts {
-  compileFunction: boolean,
+  compileFunction?: boolean,
   debug: boolean,
   captureMethod: CaptureMethod,
   newMethod: HandleNew,

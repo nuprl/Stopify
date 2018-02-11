5be57fbe04115e0d9c24852c2962e4a14d17137d
Arjun Guha <arjun@cs.umass.edu>
2018-02-11 12:50:27 -0500

    Minor fix to test-deployed

--- stopify-compiler-cloud-function/bin/test-deployed.sh
+++ stopify-compiler-cloud-function/bin/test-deployed.sh
@@ -8,5 +8,5 @@ BUCKET=`jq -r '.["output-bucket"]' $DIR/../config.json`
 LANG=$1
 SRC=$2
 
-OUTFILE=`curl -X POST -d @$SRC -H 'Content-Type: text/plain' $URL/$1`
+OUTFILE=`curl -X POST --data-binary @$SRC -H 'Content-Type: text/plain' $URL/$1`
 curl "https://storage.googleapis.com/$BUCKET/$OUTFILE"

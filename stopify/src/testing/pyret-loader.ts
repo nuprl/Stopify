edb33810e9103a85ba2c016a5828010a6f1f1ee9
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-19 12:05:43 -0500

    add separate loader for pyret

---
+++ stopify/src/testing/pyret-loader.ts
@@ -0,0 +1,9 @@
+import { parseRuntimeOpts } from "../cli-parse";
+
+const opts = parseRuntimeOpts(
+  JSON.parse(decodeURIComponent(window.location.hash.slice(1))));
+
+const url = opts.filename;
+const script = document.createElement('script');
+script.setAttribute('src', url)
+document.body.appendChild(script)

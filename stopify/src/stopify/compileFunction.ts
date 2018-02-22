6b513dae53efe455feee08ddee473d2e782d38de
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-22 11:17:19 -0500

    compileFunction has no default opts

--- stopify/src/stopify/compileFunction.ts
+++ stopify/src/stopify/compileFunction.ts
@@ -60,27 +60,16 @@ const defaultExternals = [
   "captureCC"
 ];
 
-const defaultOpts: callcc.CompilerOpts = {
-    getters: false,
-    compileFunction: true,
-    debug: false,
-    captureMethod: 'lazy',
-    newMethod: 'wrapper',
-    eval: false,
-    es: 'sane',
-    hofs: 'builtin',
-    jsArgs: 'simple',
-    requireRuntime: false,
-    externals: defaultExternals
-}
+export function compileFunction(code: string, opts: callcc.CompilerOpts): string {
 
-export function compileFunction(
-  code: string,
-  opts: callcc.CompilerOpts = defaultOpts): string {
+  // Require because compileFunction may be called from arbitrary JS.
+  assert(opts !== undefined, 'CompileFunction expects options object');
+
   const babelOpts = {
     plugins: [[() => ({ visitor }), opts]],
     babelrc: false
   };
+
   const { code:transformed } = babel.transform(code, babelOpts)
   if (!transformed) {
     throw new Error("Failed to transform function")

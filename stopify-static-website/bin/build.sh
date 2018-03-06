e42106015b9acf8df7902cdacc47242063d04087
Arjun Guha <arjun@cs.umass.edu>
2018-03-06 13:17:17 -0500

    Publish manual on website

--- stopify-static-website/bin/build.sh
+++ stopify-static-website/bin/build.sh
@@ -5,6 +5,7 @@ BROWSERIFY=./node_modules/.bin/browserify
 
 jekyll build # Must run first, or generated .js files get clobbered
 ./node_modules/.bin/tsc
+cp ../manual/manual.pdf dist
 
 $BROWSERIFY -d dist/stopify.js -o dist/stopify.js
 $BROWSERIFY -d dist/container.js -o dist/container.js

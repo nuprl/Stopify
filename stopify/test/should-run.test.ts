3783ed0c2fffd418f97e7a60dc56d8254d93c091
Rachit Nigam <rachit.nigam12@gmail.com>
2018-03-08 00:25:25 -0500

    test new wrapper again

--- stopify/test/should-run.test.ts
+++ stopify/test/should-run.test.ts
@@ -3,7 +3,7 @@ import * as f from './testFixtures.js';
 describe('call/cc', function() {
   f.unitTests.forEach(function(filename: string) {
     f.callCCTest(filename, "lazy");
-    f.callCCTest(filename, "lazy", "--new direct")
+    f.callCCTest(filename, "lazy", "--new wrapper")
     f.callCCTest(filename, "lazyDeep");
     f.callCCTest(filename, "eager");
     f.callCCTest(filename, "retval");

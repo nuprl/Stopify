903bfbc6ea5042d413b78af50bf79445bb587bac
Sam Baxter <sbaxter@cs.umass.edu>
2018-04-06 17:26:54 -0400

    Update to v0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,8 @@ source_suffix = ['.rst', '.md']
 # Specify the root of the documentation tree
 master_doc = 'index'
 project = u'Stopify User\'s Manual'
-version = '0.2.0'
-release = '0.2.0'
+version = '0.2.1'
+release = '0.2.1'
 
 # Activate the docs theme
 html_theme = 'sphinx_rtd_theme'

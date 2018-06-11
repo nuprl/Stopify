27ef40a8cf0cc81bab5e318d62aa26891610023a
Sam Baxter <sbaxter@cs.umass.edu>
2018-06-11 14:21:58 -0400

    Handle async function statements in addition to exprs

--- stopify-continuations/src/callcc/jumperizeAsync.ts
+++ stopify-continuations/src/callcc/jumperizeAsync.ts
@@ -7,24 +7,31 @@ const captureExn = t.memberExpression(t.identifier('$__T'), t.identifier('Captur
 const restoreExn = t.memberExpression(t.identifier('$__T'), t.identifier('Restore'));
 const endTurnExn = t.memberExpression(t.identifier('$__T'), t.identifier('EndTurn'));
 
+function handleAsync(path: NodePath<t.FunctionDeclaration|t.FunctionExpression>): void {
+  if (path.node.async) {
+    const { body } = path.node;
+    const param = fresh('exn');
+    const catchBody = t.ifStatement(bh.or(
+      t.binaryExpression('instanceof', param, captureExn),
+      t.binaryExpression('instanceof', param, restoreExn),
+      t.binaryExpression('instanceof', param, endTurnExn)),
+      t.returnStatement(param),
+      t.throwStatement(param));
+    const catchHandler = t.catchClause(param, t.blockStatement([
+      catchBody,
+    ]));
+    (<any>catchHandler).eVar = param;
+    const newBody = t.blockStatement([t.tryStatement(body, catchHandler)]);
+    path.node.body = newBody;
+  }
+}
+
 const visitor: Visitor = {
+  FunctionDeclaration: function (path: NodePath<t.FunctionDeclaration>): void {
+    return handleAsync(path);
+  },
   FunctionExpression: function (path: NodePath<t.FunctionExpression>): void {
-    if (path.node.async) {
-      const { body } = path.node;
-      const param = fresh('exn');
-      const catchBody = t.ifStatement(bh.or(
-        t.binaryExpression('instanceof', param, captureExn),
-        t.binaryExpression('instanceof', param, restoreExn),
-        t.binaryExpression('instanceof', param, endTurnExn)),
-        t.returnStatement(param),
-        t.throwStatement(param));
-      const catchHandler = t.catchClause(param, t.blockStatement([
-        catchBody,
-      ]));
-      (<any>catchHandler).eVar = param;
-      const newBody = t.blockStatement([t.tryStatement(body, catchHandler)]);
-      path.node.body = newBody;
-    }
+    return handleAsync(path);
   },
 };
 

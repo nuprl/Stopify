7e0c90e7494c35d9e6a894b52842db4857ed13b4
Arjun Guha <arjun@cs.umass.edu>
2019-07-12 18:26:43 -0400

    Fixed issue where normalize would not run
    The path.stop in hygiene would kill traversal in the other program!

--- normalize-js/ts/index.ts
+++ normalize-js/ts/index.ts
@@ -2,21 +2,30 @@ import * as babel from 'babel-core';
 import * as callcc from './callcc';
 import * as freeIds from './freeIds';
 import * as generic from './generic';
-
+import * as babylon from 'babylon';
 export { default as plugin } from './callcc';
 export { freeIds, generic };
 export { unreachable } from './generic';
 import * as hygiene from '@stopify/hygiene';
+import * as h from '@stopify/util';
+import { Visitor } from 'babel-traverse';
+
+const visitor : Visitor = {
+    Program(path) {
+        h.transformFromAst(path,
+            [ [ hygiene.plugin,  { reserved: [] } ] ]);
+        h.transformFromAst(path, [ callcc.default ]);
+        path.stop();
+    }
+}
 
 export function normalize(code: string) {
-    const result = babel.transform(code, {
+    let ast = babylon.parse(code);
+    const result = babel.transformFromAst(ast.program, undefined, {
         babelrc: false,
         code: true,
         ast: false,
-        plugins: [
-            [ hygiene.plugin,  { reserved: [] } ],
-            callcc.default
-        ]
+        plugins: [ () => ({ visitor }) ]
     });
     return result.code!;
 }
